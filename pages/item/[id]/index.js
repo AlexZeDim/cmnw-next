0a9967a87f3f1d13de9a96ea283b7e295bd6c3e3
Boralus Whale <alexzedim@mail.ru>
2019-10-05 21:13:25 +0300

    getInitialProps 200 GCP

--- pages/item/[id]/index.js
+++ pages/item/[id]/index.js
@@ -34,59 +34,33 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
-function createData(name, calories) {
-    return { name, calories};
-}
-
-const rows = [
-    createData('Ticker', 305),
-    createData('Type', 356),
-    createData('Asset Class', 159),
-    createData('Expansion', 356),
-    createData('Stack Size', 'x237'),
-    createData('Vendor Price', 262),
-    {'name': 'trtr', 'calories':3423}
-];
-
-const Item = ({ name }) => {
+const Item = ({asset_class, name, timestamp, market, counterparties}) => {
     const classes = useStyles();
     const router = useRouter();
     const { id } = router.query;
-    const { asset_class, ticker } = {'asset_class':'COMMDTY', 'ticker':'ZNTD'};
-
-    const test = {
-        'min': 1,
-        'min_size': 2,
-        'avg': 3,
-        'max': 4,
-        'max_size': 5,
-        'oi': 3543,
-        'quantity': 3453,
-        'cp': 6
-    };
 
     return (
         <Container fixed>
             <Typography variant="overline" display="block">
-                {asset_class}:{ticker}
+                {asset_class}:{id}
             </Typography>
             <Typography variant="h2" gutterBottom>
-                h5. {name} {id}
+                {name.ru_RU}
             </Typography>
             <Typography variant="caption" display="block">
-                timestamp
+                {timestamp}
             </Typography>
             <Divider />
                 <Grid container spacing={1}>
-                    {Object.keys(test).map(card => (
-                        <Grid item key={card} xs={12} sm={6} md={3}>
+                    {market.map((element, index) => (
+                        <Grid item key={index} xs={12} sm={6} md={4}>
                             <Card className={classes.card}>
                                 <CardContent>
                                     <Typography className={classes.title} color="textSecondary" gutterBottom>
-                                        {card}
+                                        {element.name}
                                     </Typography>
                                     <Typography variant="body2" component="p">
-                                        {test[card]}
+                                        {element.value}
                                     </Typography>
                                 </CardContent>
                             </Card>
@@ -94,18 +68,16 @@ const Item = ({ name }) => {
                     ))}
                 </Grid>
             <Divider />
-            <Divider />
             <Grid item xs={3}>
                 <div className={classes.root}>
                     <Paper className={classes.paper}>
                         <Table className={classes.table} size="small">
                             <TableBody>
-                                {rows.map(row => (
-                                    <TableRow key={row.name}>
+                                {counterparties.map(cp => (
+                                    <TableRow key={cp}>
                                         <TableCell component="th" scope="row">
-                                            {row.name}
+                                            {cp}
                                         </TableCell>
-                                        <TableCell align="right">{row.calories}</TableCell>
                                     </TableRow>
                                 ))}
                             </TableBody>
@@ -118,10 +90,20 @@ const Item = ({ name }) => {
 };
 
 Item.getInitialProps = async ({ query }) => {
-    console.log(query.id);
-    const res = await fetch(`https://directmarketaccess.ru/api/items/${query.id}`);
-    const json = await res.json();
-    return { name: json.name };
+    try {
+        const res = await fetch(`http://34.90.62.42/api/test/${query.id}@Gordunni`);
+        const json = await res.json();
+        /*
+        if (typeof json.market === 'undefined') {
+            const err = new Error();
+            err.code = 'ENOENT';
+            throw err
+        }*/
+        console.log(json);
+        return json;
+    } catch (err) {
+        return { err: { statusCode: err.status } }
+    }
 };
 
 export default Item
