47e0eddb5db0229923dc8ffb1b50e850173d35c4
Sasha <AlexZeDim@mail.ru>
2020-05-30 15:18:01 +0300

    theme palette and color schema

--- src/theme.js
+++ src/theme.js
@@ -5,18 +5,24 @@ import { red } from '@material-ui/core/colors';
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#b895d6',
+      main: '#b9b5bc',
+      light: '#ebe7ee',
+      dark: '#89858c',
     },
     secondary: {
-      main: '#19857b',
+      main: '#241c18',
+      light: '#4c433e',
+      dark: '#000000',
     },
     error: {
       main: red.A400,
     },
     background: {
-      default: '#eee',
+      default: '#ebe7ee',
     },
   },
+  contrastThreshold: 3,
+  tonalOffset: 0.2,
   typography: {
     caption: {
       fontSize: '0.9rem',
