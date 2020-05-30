5b3a2574f1b04ebfba9bb2481d8c2bb7cc6d3969
Sasha <AlexZeDim@mail.ru>
2020-05-30 19:12:10 +0300

    working searchbar

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Formik } from 'formik';
 import fetch from 'node-fetch'
 import Router from 'next/router'
-import {Container, Grid, Divider, Typography} from "@material-ui/core";
+import {Container, Grid, Divider, Typography, Select, MenuItem } from "@material-ui/core";
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from "@material-ui/core/Button";
@@ -14,24 +14,57 @@ const useStyles = makeStyles(theme => ({
     searchField: {
         margin: theme.spacing(6, 0, 6),
     },
+    searchbar: {
+        marginRight: "auto",
+        marginLeft: "auto",
+        marginTop: "20%"
+    },
     search: {
-        paddingTop: theme.spacing(15),
-        paddingBottom: theme.spacing(15),
-        height: '100%'
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+        width: '100ch',
+    },
+    dropdown: {
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+        width: '25ch',
     },
 }));
+
+const commands = [
+    {
+        value: 'character',
+        label: 'CHAR',
+    },
+    {
+        value: 'guild',
+        label: 'GUILD',
+    },
+    {
+        value: 'contract',
+        label: 'CONTRACT',
+    },
+    {
+        value: 'find',
+        label: 'FIND',
+    },
+    {
+        value: 'item',
+        label: 'ITEM',
+    },
+];
 
 function Index () {
     const classes = useStyles();
     return (
         <Grid container component="main" className={classes.root}>
-            <Container className={classes.search} maxWidth="lg">
+            <Container className={classes.searchbar} maxWidth="lg">
                 <Formik
-                    initialValues={{ searchQuery: ''}}
+                    initialValues={{ command: 'item', arguments: ''}}
                     onSubmit={async (values, { setSubmitting }) => {
-                        const [command, query] = values.searchQuery.split(/[ ]+/);
+                        console.log(values)
                         await setSubmitting(false);
-                        if (['CHAR', 'C'].includes(command.toUpperCase())) {
+/*                        if (['CHAR', 'C'].includes(command.toUpperCase())) {
                             let [nameSlug, realmSlug] = query.split('@');
                             await Router.push(`/character/${realmSlug}/${nameSlug}`);
                         } else if (['ALTS', 'ALT', 'A'].includes(command.toUpperCase())) {
@@ -47,7 +80,7 @@ function Index () {
                         } else if (['CONTRACT', 'CON'].includes(command.toUpperCase())) {
                             let [codeSlug, realmSlug] = query.split('@');
                             await Router.push(`/contract/${realmSlug}/${codeSlug}`);
-                        }
+                        }*/
                     }}
                 >
                 {({
@@ -61,15 +94,31 @@ function Index () {
                   }) => (
                     <form className={classes.searchField} onSubmit={handleSubmit} noValidate autoComplete="off">
                         <TextField
-                            type="text"
-                            name="searchQuery"
+                            name="command"
+                            select
+                            label="Select"
+                            className={classes.dropdown}
                             onChange={handleChange}
                             onBlur={handleBlur}
-                            value={values.searchQuery}
+                            value={values.command}
+                            helperText="Please select your currency"
+                        >
+                            {commands.map((option) => (
+                                <MenuItem key={option.value} value={option.value}>
+                                    {option.label}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                        <TextField
+                            type="text"
+                            name="arguments"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.arguments}
                             fullWidth id="outlined-basic"
                             label="Input Search Query"
+                            className={classes.search}
                             variant="outlined" />
-                        {errors.searchQuery && touched.searchQuery && errors.searchQuery}
                     </form>
                 )}
                 </Formik>
