1b26437f179fa47b1d4ec28e60ffa26bc87b9d9d
Sasha <AlexZeDim@mail.ru>
2020-05-31 13:02:10 +0300

    build directory

--- next.config.js
+++ next.config.js
@@ -3,11 +3,11 @@ const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 module.exports = (phase, { defaultConfig }) => {
     if (phase === PHASE_DEVELOPMENT_SERVER) {
         return {
-            distDir: 'build',
+            distDir: '.next',
         }
     }
 
     return {
-        distDir: 'build',
+        distDir: '.next',
     }
 }
