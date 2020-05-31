97f4a6c2b6bf64a4d132f52501db3831561b8f9c
Sasha <AlexZeDim@mail.ru>
2020-05-31 13:01:15 +0300

    argOne argTwo

--- pages/index.js
+++ pages/index.js
@@ -62,25 +62,9 @@ function Index () {
                 <Formik
                     initialValues={{ command: 'item', arguments: ''}}
                     onSubmit={async (values, { setSubmitting }) => {
-                        console.log(values)
+                        const [argOne, argTwo] = values.arguments.split("@")
                         await setSubmitting(false);
-/*                        if (['CHAR', 'C'].includes(command.toUpperCase())) {
-                            let [nameSlug, realmSlug] = query.split('@');
-                            await Router.push(`/character/${realmSlug}/${nameSlug}`);
-                        } else if (['ALTS', 'ALT', 'A'].includes(command.toUpperCase())) {
-                            await Router.push(`/find/${query}`);
-                        } else if (['GUILD', 'G'].includes(command.toUpperCase())) {
-                            let [guildSlug, realmSlug] = query.split('@');
-                            await Router.push(`/guild/${realmSlug}/${guildSlug}`);
-                        } else if (['MARKET', 'M'].includes(command.toUpperCase())) {
-                            let [itemSlug, realmSlug] = query.split('@');
-                            await Router.push(`/item/${realmSlug}/${itemSlug}`);
-                        } else if (['HELP', 'H'].includes(command.toUpperCase())) {
-                            await Router.push(`/help`);
-                        } else if (['CONTRACT', 'CON'].includes(command.toUpperCase())) {
-                            let [codeSlug, realmSlug] = query.split('@');
-                            await Router.push(`/contract/${realmSlug}/${codeSlug}`);
-                        }*/
+                        await Router.push(`/${values.command}/${argTwo}/${argOne}`);
                     }}
                 >
                 {({
@@ -96,12 +80,12 @@ function Index () {
                         <TextField
                             name="command"
                             select
-                            label="Select"
+                            label="Select command"
                             className={classes.dropdown}
                             onChange={handleChange}
                             onBlur={handleBlur}
                             value={values.command}
-                            helperText="Please select your currency"
+                            variant="outlined"
                         >
                             {commands.map((option) => (
                                 <MenuItem key={option.value} value={option.value}>
@@ -118,7 +102,8 @@ function Index () {
                             fullWidth id="outlined-basic"
                             label="Input Search Query"
                             className={classes.search}
-                            variant="outlined" />
+                            helperText="Select command and input your query. More info @help"
+                        />
                     </form>
                 )}
                 </Formik>
@@ -126,12 +111,5 @@ function Index () {
         </Grid>
     )
 }
-
-/*export async function getServerSideProps({query}) {
-    const {realmSlug, nameSlug} = query;
-    const res = await fetch(encodeURI(`http://localhost:3030/api/characters/${(nameSlug)}@${realmSlug}`));
-    const json = await res.json();
-    return { props: json }
-}*/
 
 export default Index
