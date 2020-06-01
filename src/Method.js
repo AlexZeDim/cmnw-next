373308ac01a766ef09af157b45b030538e0935dc
Sasha <AlexZeDim@mail.ru>
2020-06-01 05:22:14 +0300

    WIP component

--- src/Method.js
+++ src/Method.js
@@ -2,83 +2,17 @@ import React from 'react';
 import PropTypes from "prop-types";
 import MaterialTable from 'material-table';
 import { makeStyles } from '@material-ui/core/styles';
+import TableIcons from "./TableIcons"
 import {
-    Container, Grid,
-    Typography, Divider,
-    Avatar, Table,
-    TableContainer,
-    TableHead, TableRow,
-    TableCell, TableBody,
-    Paper, Chip, AppBar,
-    Tabs, Tab, Box
+    Typography, Box
 } from '@material-ui/core';
 
-import {
-    AddBox, ArrowDownward,
-    Check, ChevronLeft,
-    ChevronRight,
-    Clear,
-    DeleteOutline,
-    Edit,
-    FilterList,
-    FirstPage, LastPage, Remove,
-    SaveAlt, Search, ViewColumn
-} from "@material-ui/icons";
-import PropTypes from "prop-types";
-
-const tableIcons = {
-    Add: React.forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-    Check: React.forwardRef((props, ref) => <Check {...props} ref={ref} />),
-    Clear: React.forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    Delete: React.forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-    DetailPanel: React.forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    Edit: React.forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-    Export: React.forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-    Filter: React.forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-    FirstPage: React.forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-    LastPage: React.forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-    NextPage: React.forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    PreviousPage: React.forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
-    ResetSearch: React.forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    Search: React.forwardRef((props, ref) => <Search {...props} ref={ref} />),
-    SortArrow: React.forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-    ThirdStateCheck: React.forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-    ViewColumn: React.forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
-};
-
-const a11yProps = index => ({
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-});
 
 const useStyles = makeStyles(theme => ({
     root: {
         margin: theme.spacing(6, 0, 3),
     },
 }));
-
-const TabPanel = props => {
-    const { children, value, index, ...other } = props;
-
-    return (
-        <Typography
-            component="div"
-            role="tabpanel"
-            hidden={value !== index}
-            id={`simple-tabpanel-${index}`}
-            aria-labelledby={`simple-tab-${index}`}
-            {...other}
-        >
-            {value === index && <Box p={3}>{children}</Box>}
-        </Typography>
-    );
-};
-
-TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.any.isRequired,
-    value: PropTypes.any.isRequired,
-};
 
 export default function Method(props) {
     const classes = useStyles();
@@ -95,7 +29,7 @@ export default function Method(props) {
     });
     return (
         <MaterialTable
-            icons={tableIcons}
+            icons={TableIcons}
             title={false}
             columns={columns.columns}
             data={data.data}
