ffec236b1fd32aae16ca5df34cc8dc417926373b
AlexZeDim <AlexZeDim@mail.ru>
2020-06-09 08:23:27 +0300

    characters padding, hero

--- pages/character/[realmSlug]/[nameSlug]/index.js
+++ pages/character/[realmSlug]/[nameSlug]/index.js
@@ -15,8 +15,8 @@ const useStyles = makeStyles(theme => ({
     icon: {
         marginRight: theme.spacing(2),
     },
-    heroContent: {
-        padding: theme.spacing(6, 0, 6),
+    titleBlock: {
+        padding: theme.spacing(10, 0, 10),
     },
     heroButtons: {
         marginTop: theme.spacing(4),
@@ -64,7 +64,6 @@ const useStyles = makeStyles(theme => ({
         padding: theme.spacing(2, 4, 3),
     },
     title: {
-        color: theme.palette.primary.main,
         fontFamily: 'Fira Sans',
         fontStyle: 'normal',
         fontDisplay: 'swap',
@@ -117,7 +116,7 @@ function CharacterPage(json) {
     return (
         <main>
             {/* Hero unit */}
-            <div className={classes.heroContent}>
+            <div className={classes.titleBlock}>
                 <Container maxWidth="lg">
                     <Typography component="h1" variant="h2" align="center" color="textPrimary" className={classes.title} gutterBottom>
                         {_id}
@@ -144,128 +143,79 @@ function CharacterPage(json) {
                 </Container>
             </div>
             {/* End hero unit */}
-            <Container className={classes.cardGrid} maxWidth="lg">
+            <Container maxWidth="lg">
             {/* Cards */}
-            { (statusCode === 200) ? (
-                <Grid container spacing={4}>
-                    <Grid item key={1} xs={12} sm={6} md={3}>
-                        <Typography gutterBottom variant="overline" display="block" component="h2" className={classes.cardTitle}>
-                            Summary
-                        </Typography>
-                        <Divider light />
-                        <Typography variant="caption" display="block">
-                            ID: {id}
-                        </Typography>
-                        <Typography variant="caption" display="block">
-                            LVL: {level}
-                        </Typography>
-                    </Grid>
-                    <Grid item key={2} xs={12} sm={6} md={3}>
-                        <Typography gutterBottom variant="overline" display="block" component="h2" className={classes.cardTitle}>
-                            {faction}
-                        </Typography>
-                        <Divider light />
-                        <Typography variant="caption" display="block">
-                            {gender} / {race}
-                        </Typography>
-                        <Typography variant="caption" display="block">
-                            {character_class} / {spec}
-                        </Typography>
-                    </Grid>
-                    { (guild) ? (
-                        <Grid item key={3} xs={12} sm={6} md={3}>
-                            <Typography gutterBottom variant="overline" display="block" component="h2" className={classes.cardTitle}>
-                                Guild
-                            </Typography>
-                            <Divider light />
-                            <Typography variant="caption" display="block">
-                                {guild.name}
-                            </Typography>
-                            <Typography variant="caption" display="block">
-                                Rank { (guild.rank === 0) ? ('GM') : (guild.rank)}
-                            </Typography>
-                        </Grid>
-                        ) : ('')
-                    }
-                    { (ilvl) ? (
-                        <Grid item key={4} xs={12} sm={6} md={3}>
-                            <Typography gutterBottom variant="overline" display="block" component="h2" className={classes.cardTitle}>
-                                ilvl
-                            </Typography>
-                            <Divider light />
-                            {Object.keys(ilvl).map((key, index) => (
-                                <Typography key={index} variant="caption" display="block">
-                                    {`${key[0]}: ${ilvl[key]}`}
-                                </Typography>
-                            ))}
-                        </Grid>
-                    ) : ('')
-                    }
-                    { (hash) ? (
-                        <Grid item key={5} xs={12} sm={6} md={3}>
-                            <Typography gutterBottom variant="overline" display="block" component="h2" className={classes.cardTitle}>
-                                Hash
-                            </Typography>
-                            <Divider light />
-                            {Object.keys(hash).map((key, index) => (
-                                <Typography variant="caption" display="block">
-                                    {`${key}: ${hash[key]}`}
-                                </Typography>
-                            ))}
-                        </Grid>
-                    ) : ('')
-                    }
-                    <Grid item key={6} xs={12} sm={6} md={3}>
-                        <Typography gutterBottom variant="overline" display="block" component="h2" className={classes.cardTitle}>
-                            {updatedBy || 'OSINT-indexCharacters'}
-                        </Typography>
-                        <Divider light />
-                        <Typography variant="caption" display="block">
-                            C: {new Date(createdAt).toLocaleString('en-GB')}
-                        </Typography>
-                        <Typography variant="caption" display="block">
-                            U: {new Date(updatedAt).toLocaleString('en-GB')}
-                        </Typography>
-                    </Grid>
-                    <Grid item key={7} xs={12} sm={6} md={3}>
-                        <Typography gutterBottom variant="overline" display="block" component="h2" className={classes.cardTitle}>
-                            Timestamp
-                        </Typography>
-                        <Divider light />
-                        <Typography variant="caption" display="block">
-                            {new Date(lastModified).toLocaleString('en-GB')}
-                        </Typography>
-                        <Typography variant="caption" display="block">
-                            {new Date(lastOnline).toLocaleString('en-GB')}
-                        </Typography>
-                    </Grid>
-                    { (media) ? (
-                        <Grid item key={8} xs={12} sm={6} md={3}>
-                            <Avatar variant="rounded" alt={_id} src={media.bust_url} className={classes.large} onClick={handleOpen}/>
-                            <Modal
-                                aria-labelledby="transition-modal-title"
-                                aria-describedby="transition-modal-description"
-                                className={classes.modal}
-                                open={open}
-                                onClose={handleClose}
-                                closeAfterTransition
-                                BackdropComponent={Backdrop}
-                                BackdropProps={{
-                                    timeout: 500,
-                                }}
-                            >
-                            <Fade in={open}>
-                                <Avatar variant="rounded" alt={_id} src={media.render_url} className={classes.full}/>
-                            </Fade>
-                            </Modal>
-                        </Grid>
-                    ) : ('')
-                    }
-                </Grid>
-            ) : (
-                <Grid item key={9} xs={12} sm={6} md={3}>
+            <Grid container spacing={4}>
+                <Grid item key={1} xs={12} sm={6} md={3}>
                     <Typography gutterBottom variant="overline" display="block" component="h2" className={classes.cardTitle}>
-                        {updatedBy}
+                        Summary
+                    </Typography>
+                    <Divider light />
+                    <Typography variant="caption" display="block">
+                        ID: {id}
+                    </Typography>
+                    <Typography variant="caption" display="block">
+                        LVL: {level}
+                    </Typography>
+                </Grid>
+                <Grid item key={2} xs={12} sm={6} md={3}>
+                    <Typography gutterBottom variant="overline" display="block" component="h2" className={classes.cardTitle}>
+                        {faction}
+                    </Typography>
+                    <Divider light />
+                    <Typography variant="caption" display="block">
+                        {gender} / {race}
+                    </Typography>
+                    <Typography variant="caption" display="block">
+                        {character_class} / {spec}
+                    </Typography>
+                </Grid>
+                { (guild) ? (
+                    <Grid item key={3} xs={12} sm={6} md={3}>
+                        <Typography gutterBottom variant="overline" display="block" component="h2" className={classes.cardTitle}>
+                            Guild
+                        </Typography>
+                        <Divider light />
+                        <Typography variant="caption" display="block">
+                            {guild.name}
+                        </Typography>
+                        <Typography variant="caption" display="block">
+                            Rank { (guild.rank === 0) ? ('GM') : (guild.rank)}
+                        </Typography>
+                    </Grid>
+                    ) : ('')
+                }
+                { (ilvl) ? (
+                    <Grid item key={4} xs={12} sm={6} md={3}>
+                        <Typography gutterBottom variant="overline" display="block" component="h2" className={classes.cardTitle}>
+                            ilvl
+                        </Typography>
+                        <Divider light />
+                        {Object.keys(ilvl).map((key, index) => (
+                            <Typography key={index} variant="caption" display="block">
+                                {`${key[0]}: ${ilvl[key]}`}
+                            </Typography>
+                        ))}
+                    </Grid>
+                ) : ('')
+                }
+                { (hash) ? (
+                    <Grid item key={5} xs={12} sm={6} md={3}>
+                        <Typography gutterBottom variant="overline" display="block" component="h2" className={classes.cardTitle}>
+                            Hash
+                        </Typography>
+                        <Divider light />
+                        {Object.keys(hash).map((key, index) => (
+                            <Typography variant="caption" display="block">
+                                {`${key}: ${hash[key]}`}
+                            </Typography>
+                        ))}
+                    </Grid>
+                ) : ('')
+                }
+                <Grid item key={6} xs={12} sm={6} md={3}>
+                    <Typography gutterBottom variant="overline" display="block" component="h2" className={classes.cardTitle}>
+                        {updatedBy || 'OSINT-indexCharacters'}
                     </Typography>
                     <Divider light />
                     <Typography variant="caption" display="block">
@@ -275,8 +225,41 @@ function CharacterPage(json) {
                         U: {new Date(updatedAt).toLocaleString('en-GB')}
                     </Typography>
                 </Grid>
-                )
-            }
+                <Grid item key={7} xs={12} sm={6} md={3}>
+                    <Typography gutterBottom variant="overline" display="block" component="h2" className={classes.cardTitle}>
+                        Timestamp
+                    </Typography>
+                    <Divider light />
+                    <Typography variant="caption" display="block">
+                        {new Date(lastModified).toLocaleString('en-GB')}
+                    </Typography>
+                    <Typography variant="caption" display="block">
+                        {new Date(lastOnline).toLocaleString('en-GB')}
+                    </Typography>
+                </Grid>
+                { (media) ? (
+                    <Grid item key={8} xs={12} sm={6} md={3}>
+                        <Avatar variant="rounded" alt={_id} src={media.bust_url} className={classes.large} onClick={handleOpen}/>
+                        <Modal
+                            aria-labelledby="transition-modal-title"
+                            aria-describedby="transition-modal-description"
+                            className={classes.modal}
+                            open={open}
+                            onClose={handleClose}
+                            closeAfterTransition
+                            BackdropComponent={Backdrop}
+                            BackdropProps={{
+                                timeout: 500,
+                            }}
+                        >
+                        <Fade in={open}>
+                            <Avatar variant="rounded" alt={_id} src={media.render_url} className={classes.full}/>
+                        </Fade>
+                        </Modal>
+                    </Grid>
+                ) : ('')
+                }
+            </Grid>
             {/* End Cards */}
             { (logs && logs.length > 0) ? (
                 <TableContainer className={classes.guild_history} component={Paper}>
