42eb4cd985d7a4091e006b0aad117ea1194ddf91
AlexZeDim <AlexZeDim@mail.ru>
2020-06-11 10:19:23 +0300

    contracts W and M charts

--- pages/contract/[realmSlug]/[code]/index.js
+++ pages/contract/[realmSlug]/[code]/index.js
@@ -1,20 +1,17 @@
 import React from "react";
-import { useRouter } from 'next/router'
 import { makeStyles } from '@material-ui/core/styles';
 import Highcharts from 'highcharts'
 import HighchartsExporting from 'highcharts/modules/exporting'
 import HighchartsReact from 'highcharts-react-official'
 import HC_heatmap from "highcharts/modules/heatmap";
 import Clock from "../../../../src/Clock";
+import Link from "../../../../src/Link";
 import {
-    Container, Grid,
+    Container, Grid, Chip,
     Typography, Divider,
-    Card, CardContent,
-    Avatar, Table,
-    TableContainer,
+    Table, TableContainer,
     TableHead, TableRow,
-    TableCell, TableBody,
-    Paper, Box, Button
+    TableCell, TableBody
 } from '@material-ui/core';
 
 if (typeof Highcharts === 'object') {
@@ -43,9 +40,10 @@ const useStyles = makeStyles(theme => ({
         transform: 'scale(0.8)',
     },
     table: {
-        minWidth: 300
+        minWidth: 300,
+        textTransform: 'uppercase'
     },
-    pos: {
+    divider: {
         margin: theme.spacing(2),
     },
     title: {
@@ -70,58 +68,111 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
-const Contract = ({_id, code, realm, open_interest, price, price_size, quantity, risk, sellers, orders, type, createdAt, data, updatedAt}) => {
+const Contract = ({_id, code, item_id, realm, open_interest, price, price_size, quantity, risk, sellers, orders, type, createdAt, data, updatedAt}) => {
     const classes = useStyles();
     let tableSet = new Set();
-    data.map(({orders}) => {if (orders) orders.map(({id}) => tableSet.add(id))});
-    const chartOptions = {
-        chart: {
-            type: 'line',
-            marginTop: 40,
-            marginBottom: 120,
-            height: (9 / 16 * 50) + '%',
-            backgroundColor: 'transparent',
-        },
-        yAxis: [
-            {
-                labels: {
-                    format: '{value}g',
+
+    String.prototype.capitalize = function() {
+        return this.charAt(0).toUpperCase() + this.slice(1).replace (/_/g, " ");
+    }
+
+    let chartOptions = {};
+    if (type === "D") {
+        data.map(({orders}) => {if (orders) orders.map(({id}) => tableSet.add(id))})
+        chartOptions = {
+            chart: {
+                type: 'line',
+                marginTop: 40,
+                marginBottom: 120,
+                height: (9 / 16 * 50) + '%',
+                backgroundColor: 'transparent',
+            },
+            yAxis: [
+                {
+                    labels: {
+                        format: '{value}g',
+                    },
+                    title: {
+                        text: 'Price',
+                    }
                 },
-                title: {
-                    text: 'Price',
+                {
+                    title: {
+                        text: 'Quantity',
+                    },
+                    labels: {
+                        format: 'x{value}',
+                    },
+                    opposite: true
+                },
+                {
+                    title: {
+                        text: 'Open Interest',
+                    },
+                    labels: {
+                        format: '{value} g',
+                    },
+                    opposite: true
                 }
+            ],
+            title: {
+                text: ''
             },
-            {
-                title: {
-                    text: 'Quantity',
-                },
-                labels: {
-                    format: 'x{value}',
-                },
-                opposite: true
+            series: [
+                { name: 'Open Interest', type: 'column', data: data.map(({open_interest}) => open_interest), yAxis: 2, opacity: 0.75, color: '#89858c' },
+                { name: 'Quantity', type: 'column', data: data.map(({quantity}) => quantity), yAxis: 1, opacity: 0.75, color: '#c1aa82' },
+                { name: 'Price', data: data.map(({price}) => price), color: '#241c18' }
+            ],
+            xAxis: {
+                categories: data.map(({_id}) => (`${new Date(_id).toLocaleString('en-GB')}`))
             },
-            {
-                title: {
-                    text: 'Open Interest',
-                },
-                labels: {
-                    format: '{value} g',
-                },
-                opposite: true
+        };
+    } else {
+        data.forEach((day_week) => Object.keys(day_week).map(key => {
+            if (key === 'price' || key === 'quantity' || key === 'open_interest' || key === 'orders') {
+                chartOptions[key] = {
+                    chart: {
+                        type: 'line',
+                        marginTop: 40,
+                        marginBottom: 120,
+                        height: (9 / 16 * 50) + '%',
+                        backgroundColor: 'transparent',
+                    },
+                    yAxis: [
+                        {
+                            labels: {
+                                format: '{value}',
+                            },
+                            title: {
+                                text: key.capitalize(),
+                            }
+                        },
+                        {
+                            title: {
+                                text: 'Change',
+                            },
+                            labels: {
+                                format: '{value} %',
+                            },
+                            opposite: true
+                        }
+                    ],
+                    title: {
+                        text: ''
+                    },
+                    series: [
+                        { name: `${key.capitalize()} Low`, data: data.map((t) => t[key].low), color: '#241c18' },
+                        { name: `${key.capitalize()} High`, data: data.map((t) => t[key].high), color: '#241c18' },
+                        { name: `${key.capitalize()} Change`, type: 'column', data: data.map((t) => parseFloat(((t[key].change/t[key].close)*100).toFixed(2))), yAxis: 1, opacity: 0.75, color: '#c1aa82' },
+                    ],
+                    xAxis: {
+                        categories: data.map(({_id}) => (`${new Date(_id).toLocaleDateString('en-GB')}`))
+                    },
+                }
             }
-        ],
-        title: {
-            text: ''
-        },
-        series: [
-            { name: 'Open Interest', type: 'column', data: data.map(({open_interest}) => open_interest), yAxis: 2, opacity: 0.75, color: '#89858c' },
-            { name: 'Quantity', type: 'column', data: data.map(({quantity}) => quantity), yAxis: 1, opacity: 0.75, color: '#c1aa82' },
-            { name: 'Price', data: data.map(({price}) => price), color: '#241c18' }
-        ],
-        xAxis: {
-            categories: data.map(({_id}) => (`${new Date(_id).toLocaleString('en-GB')}`))
-        },
-    };
+        }))
+    }
+
     return (
         <Container maxWidth={false}>
             <Container maxWidth={false} className={classes.titleBlock}>
@@ -136,10 +187,12 @@ const Contract = ({_id, code, realm, open_interest, price, price_size, quantity,
                                 {code}@{realm.name}
                             </Typography>
                         )}
-
                     </Grid>
                     <Grid item xs>
                         <Clock time={updatedAt}/>
+                    </Grid>
+                    <Grid item xs>
+                        <Chip clickable color="default" variant="default" className={classes.chip} label={<Link href={`/item/${realm.connected_realm_id}/${item_id}`} color="inherit" underline="none">UNDERLYING ASSET</Link>}/>
                     </Grid>
                 </Grid>
             </Container>
@@ -217,7 +270,7 @@ const Contract = ({_id, code, realm, open_interest, price, price_size, quantity,
                                         <TableCell component="th" scope="row">
                                             {key.replace(/_/g, ' ')}
                                         </TableCell>
-                                        <TableCell align="right">{value.toLocaleString('ru-RU')}</TableCell>
+                                        <TableCell align="right">{Math.round(value).toLocaleString('ru-RU')}</TableCell>
                                     </TableRow>
                                 ))}
                             </TableBody>
@@ -229,10 +282,10 @@ const Contract = ({_id, code, realm, open_interest, price, price_size, quantity,
                         <Table className={classes.table} size="small" aria-label="OrdersSellers">
                             <TableHead>
                                 <TableRow>
-                                    <TableCell>{(tableSet.size > 0) ? ('Orders') : ('Sellers')}</TableCell>
+                                    <TableCell>{(orders) ? ('Orders') : ('Sellers')}</TableCell>
                                 </TableRow>
                             </TableHead>
-                            {(tableSet.size > 0) ? (
+                            {(orders) ? (
                                 <TableBody>
                                     {Object.entries(orders).map(([key, value],i) => (
                                         <TableRow key={i}>
@@ -284,10 +337,10 @@ const Contract = ({_id, code, realm, open_interest, price, price_size, quantity,
                         <Table stickyHeader className={classes.table} size="small" aria-label="TableO&S">
                             <TableHead>
                                 <TableRow>
-                                    <TableCell>{(tableSet.size > 0) ? ('Order Log') : ('Sellers Log')}</TableCell>
+                                    <TableCell>{(orders) ? ('Order Log') : ('Sellers Log')}</TableCell>
                                 </TableRow>
                             </TableHead>
-                            {(tableSet.size > 0) ? (
+                            {(orders) ? (
                                 <TableBody>
                                     {Array.from(tableSet).map((value, i) => (
                                         <TableRow key={i}>
@@ -302,7 +355,7 @@ const Contract = ({_id, code, realm, open_interest, price, price_size, quantity,
                                     {sellers.sellers.map((value, i) => (
                                         <TableRow key={i}>
                                             <TableCell component="th" scope="row">
-                                                {value}
+                                                {value}f
                                             </TableCell>
                                         </TableRow>
                                     ))}
@@ -312,16 +365,32 @@ const Contract = ({_id, code, realm, open_interest, price, price_size, quantity,
                     </TableContainer>
                 </Grid>
             </Grid>
-            <Divider className={classes.pos} />
-            <Grid container>
-                <Grid item xs={12}>
-                <HighchartsReact
-                    highcharts={Highcharts}
-                    options={chartOptions}
-                />
+            {(type === 'D') ? (
+                <Grid container>
+                    <Divider className={classes.divider} />
+                    <Grid item xs={12}>
+                        <HighchartsReact
+                            highcharts={Highcharts}
+                            options={chartOptions}
+                        />
+                    </Grid>
                 </Grid>
-            </Grid>
-            <Divider className={classes.pos} />
+            ) : (
+                <Grid container>
+                    {Object.keys(chartOptions).map(key => (
+                        <React.Fragment>
+                            <Divider className={classes.divider} />
+                            <Grid item xs={12}>
+                                <HighchartsReact
+                                    highcharts={Highcharts}
+                                    options={chartOptions[key]}
+                                />
+                            </Grid>
+                        </React.Fragment>
+                    ))}
+                </Grid>
+            )}
+            <Divider className={classes.divider} />
         </Container>
     )
 };
