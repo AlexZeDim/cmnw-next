cef5c1081f1f9d8d9c44d9ef7689a4f897c09f49
AlexZeDim <AlexZeDim@mail.ru>
2020-06-11 10:19:49 +0300

    find w/o Material Tables

--- pages/find/[type]/[match]/index.js
+++ pages/find/[type]/[match]/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import Link from "../../../../src/Link";
-import TableIcons from "../../../../src/TableIcons"
-import MaterialTable from 'material-table';
-import { Container, Typography } from "@material-ui/core";
+import {
+    Container, Typography,
+    Table, TableContainer,
+    TableHead, TableRow,
+    TableCell, TableBody, Paper, Grid,
+} from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
     icon: {
@@ -15,9 +18,8 @@ const useStyles = makeStyles(theme => ({
     heroButtons: {
         marginTop: theme.spacing(4),
     },
-    cardGrid: {
-        paddingTop: theme.spacing(4),
-        paddingBottom: theme.spacing(4),
+    titleBlock: {
+        padding: theme.spacing(10, 0, 10),
     },
     card: {
         height: '100%',
@@ -63,51 +65,47 @@ const useStyles = makeStyles(theme => ({
 function CharacterPage({ _id, match }) {
     const classes = useStyles();
     return (
-        <React.Fragment>
-            <main>
-                {/* Hero unit */}
-                <div className={classes.heroContent}>
-                    <Container maxWidth="lg">
-                        <Typography component="h1" variant="h2" align="center" color="secondary" className={classes.title} gutterBottom>
-                            {_id}
-                        </Typography>
-                    </Container>
-                </div>
-                {/* End hero unit */}
-                <Container className={classes.cardGrid} maxWidth={false}>
-                    <MaterialTable
-                        title="Render Image Preview"
-                        icons={TableIcons}
-                        columns={[
-                            { title: 'Avatar', field: 'media', render: ({media}) => <img src={media.avatar_url} style={{width: 50, borderRadius: '50%'}}/> },
-                            { title: 'Name', field: 'name' },
-                            {
-                                field: 'realm',
-                                title: 'Realm',
-                                render: ({realm}) => realm.name
-                            },
-                            {
-                                field: 'guild',
-                                title: 'Guild',
-                                render: ({realm, guild}) => <Link href={encodeURI(`/guild/${realm.slug}/${guild.slug}`)} color="secondary" underline="hover">{guild.name}</Link>
-                            },
-                            { title: 'Class', field: 'character_class' },
-                            { title: 'Level', field: 'level' },
-                            { title: 'Faction', field: 'faction' },
-                            { title: 'Race', field: 'race' },
-                            { title: 'Gender', field: 'gender' },
-                        ]}
-                        data={match}
-                        options={{
-                            sorting: true,
-                            pageSize: 10,
-                            pageSizeOptions: [5,10,25,50],
-                            showTitle: false,
-                        }}
-                    />
+        <main>
+            <div className={classes.titleBlock}>
+                <Container maxWidth="lg">
+                    <Typography component="h1" variant="h2" align="center" color="textPrimary" className={classes.title} gutterBottom>
+                        {_id}
+                    </Typography>
                 </Container>
-            </main>
-        </React.Fragment>
+            </div>
+            <Container className={classes.cardGrid} maxWidth={false}>
+                <TableContainer>
+                    <Table stickyHeader size="small" aria-label="MAtch">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell></TableCell>
+                                <TableCell align="left">Name@Realm</TableCell>
+                                <TableCell align="left">Guild</TableCell>
+                                <TableCell align="left">Class</TableCell>
+                                <TableCell align="left">Level</TableCell>
+                                <TableCell align="left">Faction</TableCell>
+                                <TableCell align="left">Race</TableCell>
+                                <TableCell align="left">Gender</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {match.map(({_id, avatar, name, realm, guild, character_class, level, faction, race, gender, media}, i) => (
+                                <TableRow key={i}>
+                                    <TableCell component="th" scope="row">{(media) ? (<img src={media.avatar_url} style={{width: 50, borderRadius: '50%'}} alt="A"/>) : ("")}</TableCell>
+                                    <TableCell align="left" style={{textTransform: 'uppercase'}}>{<Link href={encodeURI(`/character/${realm.slug}/${name}`)} color="secondary" underline="hover">{_id}</Link>}</TableCell>
+                                    <TableCell align="left">{(guild) ? (<Link href={encodeURI(`/guild/${realm.slug}/${guild.slug}`)} color="secondary" underline="hover">{guild.name}</Link>) : ("")}</TableCell>
+                                    <TableCell align="left">{(character_class) ? (character_class) : ("")}</TableCell>
+                                    <TableCell align="left">{(level) ? (level) : ("")}</TableCell>
+                                    <TableCell align="left">{(faction) ? (faction) : ("")}</TableCell>
+                                    <TableCell align="left">{(race) ? (race) : ("")}</TableCell>
+                                    <TableCell align="left">{(gender) ? (gender) : ("")}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Container>
+        </main>
     )
 }
 
