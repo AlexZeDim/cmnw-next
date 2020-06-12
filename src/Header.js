6fb4677ede4918ff2ac0fec2c365a9a3772ff75b
AlexZeDim <AlexZeDim@mail.ru>
2020-06-12 08:20:00 +0300

    Header optimization CSS

--- src/Header.js
+++ src/Header.js
@@ -2,11 +2,8 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
 import { fade, makeStyles } from '@material-ui/core/styles';
-import SearchIcon from '@material-ui/icons/Search';
 import Link from "./Link";
-import { palette } from '@material-ui/system';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -16,53 +13,14 @@ const useStyles = makeStyles(theme => ({
         backgroundColor: fade(theme.palette.common.black, 0.75),
         position: 'relative',
     },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
     title: {
         flexGrow: 1,
         display: 'none',
+        textTransform: 'uppercase',
         [theme.breakpoints.up('sm')]: {
             display: 'block',
         },
-    },
-    search: {
-        position: 'relative',
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: fade(theme.palette.common.white, 0.15),
-        '&:hover': {
-            backgroundColor: fade(theme.palette.common.white, 0.25),
-        },
-        marginLeft: 0,
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            marginLeft: theme.spacing(1),
-            width: 'auto',
-        },
-    },
-    searchIcon: {
-        width: theme.spacing(7),
-        height: '100%',
-        position: 'absolute',
-        pointerEvents: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    inputRoot: {
-        color: 'inherit',
-    },
-    inputInput: {
-        padding: theme.spacing(1, 1, 1, 7),
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            width: 120,
-            '&:focus': {
-                width: 200,
-            },
-        },
-    },
+    }
 }));
 
 export default function Header() {
@@ -74,32 +32,19 @@ export default function Header() {
                 <Toolbar>
                     <Typography className={classes.title} variant="h6" color="primary" noWrap>
                         <Link href="/" color="inherit" underline="none">
-                            TERMINAL
+                            Terminal
                         </Link>
                     </Typography>
                     <Typography className={classes.title} variant="h6" color="primary" noWrap>
                         <Link href="/help" color="inherit" underline="none">
-                            HELP
+                            Help
                         </Link>
                     </Typography>
                     <Typography className={classes.title} variant="h6" color="primary" noWrap>
                         <Link href="/who-we-are" color="inherit" underline="none">
-                            WHO WE ARE
+                            Who we are
                         </Link>
                     </Typography>
-                    <div className={classes.search}>
-                        <div className={classes.searchIcon}>
-                            <SearchIcon />
-                        </div>
-                        <InputBase
-                            placeholder="Search…"
-                            classes={{
-                                root: classes.inputRoot,
-                                input: classes.inputInput,
-                            }}
-                            inputProps={{ 'aria-label': 'search' }}
-                        />
-                    </div>
                 </Toolbar>
             </AppBar>
         </div>
