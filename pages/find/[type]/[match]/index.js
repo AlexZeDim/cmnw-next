ca3bb42221110d8adc33fdf7d73ae514a5b00777
AlexZeDim <AlexZeDim@mail.ru>
2020-06-12 05:45:52 +0300

    FIND page

--- pages/find/[type]/[match]/index.js
+++ pages/find/[type]/[match]/index.js
@@ -1,43 +1,13 @@
 import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import Link from "../../../../src/Link";
-import {
-    Container, Typography,
-    Table, TableContainer,
-    TableHead, TableRow,
-    TableCell, TableBody, Paper, Grid,
-} from "@material-ui/core";
+import TableIcons from "../../../../src/TableIcons"
+import MaterialTable from 'material-table';
+import { Container, Typography } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
-    icon: {
-        marginRight: theme.spacing(2),
-    },
-    heroContent: {
-        padding: theme.spacing(6, 0, 6),
-    },
-    heroButtons: {
-        marginTop: theme.spacing(4),
-    },
     titleBlock: {
         padding: theme.spacing(10, 0, 10),
-    },
-    card: {
-        height: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-    },
-    cardMedia: {
-        paddingTop: '56.25%', // 16:9
-    },
-    cardContent: {
-        flexGrow: 1,
-    },
-    footer: {
-        backgroundColor: theme.palette.background.paper,
-        padding: theme.spacing(6),
-    },
-    table: {
-        minWidth: 650,
     },
     title: {
         fontFamily: 'Fira Sans',
@@ -45,21 +15,7 @@ const useStyles = makeStyles(theme => ({
         fontDisplay: 'swap',
         fontWeight: 400,
         textTransform: 'uppercase'
-    },
-    findAllResult: {
-        marginTop: theme.spacing(0),
-    },
-    modal: {
-        margin: theme.spacing(30, 60, 30),
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    paper: {
-        backgroundColor: theme.palette.background.paper,
-        border: '2px solid #000',
-        boxShadow: theme.shadows[5],
-        padding: theme.spacing(2, 4, 3),
-    },
+    }
 }));
 
 function CharacterPage({ _id, match }) {
@@ -68,42 +24,47 @@ function CharacterPage({ _id, match }) {
         <main>
             <div className={classes.titleBlock}>
                 <Container maxWidth="lg">
-                    <Typography component="h1" variant="h2" align="center" color="textPrimary" className={classes.title} gutterBottom>
+                    <Typography component="h1" variant="h2" align="center" color="secondary" className={classes.title} gutterBottom>
                         {_id}
                     </Typography>
                 </Container>
             </div>
-            <Container className={classes.cardGrid} maxWidth={false}>
-                <TableContainer>
-                    <Table stickyHeader size="small" aria-label="MAtch">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell></TableCell>
-                                <TableCell align="left">Name@Realm</TableCell>
-                                <TableCell align="left">Guild</TableCell>
-                                <TableCell align="left">Class</TableCell>
-                                <TableCell align="left">Level</TableCell>
-                                <TableCell align="left">Faction</TableCell>
-                                <TableCell align="left">Race</TableCell>
-                                <TableCell align="left">Gender</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {match.map(({_id, avatar, name, realm, guild, character_class, level, faction, race, gender, media}, i) => (
-                                <TableRow key={i}>
-                                    <TableCell component="th" scope="row">{(media) ? (<img src={media.avatar_url} style={{width: 50, borderRadius: '50%'}} alt="A"/>) : ("")}</TableCell>
-                                    <TableCell align="left" style={{textTransform: 'uppercase'}}>{<Link href={encodeURI(`/character/${realm.slug}/${name}`)} color="secondary" underline="hover">{_id}</Link>}</TableCell>
-                                    <TableCell align="left">{(guild) ? (<Link href={encodeURI(`/guild/${realm.slug}/${guild.slug}`)} color="secondary" underline="hover">{guild.name}</Link>) : ("")}</TableCell>
-                                    <TableCell align="left">{(character_class) ? (character_class) : ("")}</TableCell>
-                                    <TableCell align="left">{(level) ? (level) : ("")}</TableCell>
-                                    <TableCell align="left">{(faction) ? (faction) : ("")}</TableCell>
-                                    <TableCell align="left">{(race) ? (race) : ("")}</TableCell>
-                                    <TableCell align="left">{(gender) ? (gender) : ("")}</TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
+            <Container maxWidth={false}>
+                <MaterialTable
+                    title="Match based in ${_id}"
+                    icons={TableIcons}
+                    columns={[
+                        { title: 'Avatar', field: 'media', render: ({media}) => ((media) ? (<img src={media.avatar_url} style={{width: 50, borderRadius: '50%'}}/>) : (""))},
+                        { title: 'Name', field: 'name', render: ({name, realm}) => (<Link href={encodeURI(`/character/${realm.slug}/${name}`)} color="secondary" underline="hover">{name}</Link>) },
+                        {
+                            field: 'realm',
+                            title: 'Realm',
+                            render: ({realm}) => realm.name
+                        },
+                        {
+                            field: 'guild',
+                            title: 'Guild',
+                            render: ({realm, guild}) => ((realm && guild) ? (<Link href={encodeURI(`/guild/${realm.slug}/${guild.slug}`)} color="secondary" underline="hover">{guild.name}</Link>) : (""))
+                        },
+                        { title: 'Class', field: 'character_class' },
+                        { title: 'Level', field: 'level' },
+                        { title: 'Faction', field: 'faction' },
+                        { title: 'Race', field: 'race' },
+                        { title: 'Gender', field: 'gender' },
+                    ]}
+                    data={match}
+                    style={{
+                        backgroundColor: '#ebe7ee',
+                        marginBottom: "32px",
+                    }}
+                    options={{
+                        sorting: true,
+                        pageSize: 10,
+                        pageSizeOptions: [5,10,25,50],
+                        showTitle: false,
+                        headerStyle: {backgroundColor:'#ebe7ee', padding: '5px'}
+                    }}
+                />
             </Container>
         </main>
     )
