bf111051b85bd2eb72cf593d0d7f1a28c30e19be
AlexZeDim <AlexZeDim@mail.ru>
2020-08-05 14:02:11 +0300

    refactoring guild data

--- pages/guild/[realmSlug]/[guildSlug]/index.js
+++ pages/guild/[realmSlug]/[guildSlug]/index.js
@@ -1,82 +1,25 @@
 import React from "react";
-import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
-import Link from "../../../../src/Link";
-import TableIcons from "../../../../src/TableIcons"
-import MaterialTable from 'material-table';
 import {
-    Container, Grid,
-    Divider, Typography,
-    AppBar, Paper, Box, Tabs, Tab,
+    Grid, Divider, Typography,
 } from "@material-ui/core";
-
-
-function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-
-    return (
-        <Typography
-            component="div"
-            role="tabpanel"
-            hidden={value !== index}
-            id={`simple-tabpanel-${index}`}
-            aria-labelledby={`simple-tab-${index}`}
-            {...other}
-        >
-            {value === index && <Box p={3}>{children}</Box>}
-        </Typography>
-    );
-}
-
-TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.any.isRequired,
-    value: PropTypes.any.isRequired,
-};
-
-function a11yProps(index) {
-    return {
-        id: `simple-tab-${index}`,
-        'aria-controls': `simple-tabpanel-${index}`,
-    };
-}
+import GuildMembers from "../../../../src/GuildMembers";
+import OSINT_Logs from "../../../../src/OsintLogs";
 
 const useStyles = makeStyles(theme => ({
     paper: {
-        width: '100%',
+        marginTop: theme.spacing(2),
+        marginBottom: theme.spacing(4),
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    hr: {
+        marginTop: theme.spacing(1),
+        marginRight: theme.spacing(2),
         marginBottom: theme.spacing(2),
     },
-    root: {
-        marginTop: theme.spacing(2),
-    },
-    icon: {
-        marginRight: theme.spacing(2),
-    },
-    heroContent: {
-        marginTop: theme.spacing(10),
-        padding: theme.spacing(6, 0, 6),
-    },
-    heroButtons: {
-        marginTop: theme.spacing(4),
-    },
-    cardGrid: {
-        paddingTop: theme.spacing(2),
-        paddingBottom: theme.spacing(2),
-    },
-    card: {
-        height: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-    },
-    cardMedia: {
-        paddingTop: '56.25%', // 16:9
-    },
-    cardContent: {
-        flexGrow: 1,
-    },
-    footer: {
-        backgroundColor: theme.palette.background.paper,
-        padding: theme.spacing(6),
+    left: {
+        paddingLeft: theme.spacing(3),
     },
     title: {
         fontFamily: 'Fira Sans',
@@ -84,189 +27,76 @@ const useStyles = makeStyles(theme => ({
         fontDisplay: 'swap',
         fontWeight: 400,
         textTransform: 'uppercase'
-    },
-    cardTitle: {
-        fontSize: '1.1em',
-        fontWeight: 600
-    },
+    }
 }));
 
-function GuildPage({
-       id, name, realm,
-       updatedBy, guild_log,
-       members, achievement_points,
-       created_timestamp, member_count,
-       //crest,
-       createdAt, updatedAt
-    }) {
-    const classes = useStyles();
-    const [value, setValue] = React.useState(0);
-    let join, leave, demote, promote;
-    if (guild_log) ({join, leave, demote, promote} = guild_log);
+function GuildPage({guild}) {
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
+    const [ info, logs ] = guild;
+
+    let name,
+        realm,
+        members,
+        created_timestamp
+
+    if (info.value) {
+        ({
+            name,
+            realm,
+            members,
+            created_timestamp
+        } = info.value);
+    }
+
+    /**
+     * TODO
+     * + add more guild information, faction!
+     * + xs 12 as 6 + 6 for lg
+     * + guild crest
+     */
+
+    const classes = useStyles();
 
     return (
         <main>
-            {/* Hero unit */}
-            <div className={classes.heroContent}>
-                <Container maxWidth="lg">
-                    <Typography component="h1" variant="h2" align="center" color="secondary" className={classes.title} gutterBottom>
-                        {name}@{realm.name}
-                    </Typography>
-                </Container>
-            </div>
-            {/* End hero unit */}
-            <Container className={classes.cardGrid} maxWidth="lg">
-                <Grid container spacing={4}>
-                    <Grid item key={0} xs={12} sm={6} md={6}>
-                        <Typography gutterBottom variant="overline" display="block" component="h2" className={classes.cardTitle}>
-                            Summary
-                        </Typography>
-                        <Divider light />
-                        <Typography variant="caption" display="block">
-                            ID: {id}
-                        </Typography>
-                        <Typography variant="caption" display="block">
-                            Achievements: {achievement_points}
-                        </Typography>
-                        <Typography variant="caption" display="block">
-                            Members: {member_count}
-                        </Typography>
+            <Grid container>
+                <Grid item xs={12} sm={12} md={12}>
+                    <div className={classes.paper}>
+                        <Grid container direction="column" justify="center" alignItems="center">
+                            <Typography variant="h3" component="h3" color="textPrimary" className={classes.title}>
+                                #{name}
+                            </Typography>
+                            <Typography variant="h4" component="h4" color="textPrimary" className={classes.title}>
+                                @{realm.name}
+                            </Typography>
+                            <Typography variant="overline">
+                                Created: {new Date(created_timestamp).toLocaleString('en-GB')}
+                            </Typography>
+                        </Grid>
+                        <Divider light className={classes.hr}/>
+                    </div>
+                </Grid>
+                <Grid container className={classes.left} item xs={12} sm={12} md={12} spacing={5}>
+                    <Grid container item xs={12} sm={6} md={6}>
+                        <GuildMembers data={members}/>
                     </Grid>
-                    <Grid item key={1} xs={12} sm={6} md={6}>
-                        <Typography gutterBottom variant="overline" display="block" component="h2" className={classes.cardTitle}>
-                            {updatedBy}
-                        </Typography>
-                        <Divider />
-                        <Typography variant="caption" display="block">
-                            Founded: {new Date(created_timestamp).toLocaleString('en-GB')}
-                        </Typography>
-                        <Typography variant="caption" display="block">
-                            Indexed: {new Date(createdAt).toLocaleString('en-GB')}
-                        </Typography>
-                        <Typography variant="caption" display="block">
-                            Updated: {new Date(updatedAt).toLocaleString('en-GB')}
-                        </Typography>
+                    <Grid container item xs={12} sm={6} md={6}>
+                        <OSINT_Logs data={logs.value} pageSize={15}/>
                     </Grid>
                 </Grid>
-                <div className={classes.root}>
-                    <AppBar position="static" color="primary">
-                        <Tabs
-                            value={value}
-                            onChange={handleChange}
-                            aria-label="GuildLog"
-                            centered
-                        >
-                            <Tab label="Members" {...a11yProps(0)} />
-                        </Tabs>
-                    </AppBar>
-                    <TabPanel value={value} index={0}>
-                        <Paper className={classes.paper}>
-                            <MaterialTable
-                                title="Basic Sorting Preview"
-                                icons={TableIcons}
-                                columns={[
-                                    {
-                                        field: 'url_id',
-                                        title: 'Name',
-                                        render: rowData => <Link href={rowData.url_id} color="secondary" underline="hover">{rowData._id}</Link>
-                                    },
-                                    {
-                                        title: 'Rank',
-                                        field: 'rank',
-                                        type: 'numeric',
-                                        defaultSort: 'asc',
-                                        render: rowData => (parseInt(rowData.rank) === 0) ? ('GM') : (rowData.rank)
-                                    },
-                                    {
-                                        field: 'url_a',
-                                        title: 'Hash A',
-                                        render: rowData => <Link href={rowData.url_a} color="secondary" underline="hover">{rowData.a}</Link>
-                                    },
-                                    {
-                                        field: 'url_b',
-                                        title: 'Hash B',
-                                        render: rowData => <Link href={rowData.url_b} color="secondary" underline="hover">{rowData.b}</Link>
-                                    },
-                                    {
-                                        field: 'url_c',
-                                        title: 'Hash C',
-                                        render: rowData => <Link href={rowData.url_c} color="secondary" underline="hover">{rowData.c}</Link>
-                                    },
-                                    {
-                                        field: 'url_ex',
-                                        title: 'Hash EX',
-                                        render: rowData => <Link href={rowData.url_ex} color="secondary" underline="hover">{rowData.ex}</Link>
-                                    },
-                                    {
-                                        title: 'Last Modified',
-                                        field: 'lastModified',
-                                        cellStyle: {
-                                            width: 300,
-                                            minWidth: 300
-                                        },
-                                        render: rowData => new Date(rowData.lastModified).toLocaleString('en-GB')
-                                    },
-                                ]}
-                                data={members.map(({_id, name, realm, guild, hash, lastModified}) => {
-                                    let row = {
-                                        _id: _id,
-                                        url_id: `/character/${realm.name}/${name}`,
-                                        rank: `${guild.rank}`,
-                                        a: 0,
-                                        url_a: ``,
-                                        b: 0,
-                                        url_b: ``,
-                                        c: 0,
-                                        url_c: ``,
-                                        ex: 0,
-                                        url_ex: ``,
-                                        lastModified: lastModified
-                                    };
-                                    if (hash) {
-                                        if ("a" in hash) {
-                                            row.a = hash.a;
-                                            row.url_a = `/find/a/${hash.a}`;
-                                        }
-                                        if ("b" in hash) {
-                                            row.b = hash.b;
-                                            row.url_b = `/find/b/${hash.b}`;
-                                        }
-                                        if ("c" in hash) {
-                                            row.c = hash.c;
-                                            row.url_c = `/find/c/${hash.c}`;
-                                        }
-                                        if ("ex" in hash) {
-                                            row.ex = hash.ex;
-                                            row.url_ex = `/find/ex/${hash.ex}`;
-                                        }
-                                    }
-                                    return row
-                                })}
-                                style={{textTransform: "uppercase"}}
-                                options={{
-                                    sorting: true,
-                                    pageSize: 20,
-                                    pageSizeOptions: [10,25,50],
-                                    showTitle: false
-                                }}
-                            />
-                        </Paper>
-                    </TabPanel>
-                </div>
-            </Container>
+            </Grid>
         </main>
     )
 }
 
 export async function getServerSideProps({query}) {
-    const {realmSlug, guildSlug} = query;
-    const res = await fetch(encodeURI(`http://${process.env.api}/guilds/${(guildSlug)}@${realmSlug}`));
-    const json = await res.json();
-    return { props: json}
+    const { realmSlug, guildSlug } = query;
+
+    const guild = await Promise.allSettled([
+        fetch(encodeURI(`http://${process.env.api}/guilds/guild/${(guildSlug)}@${realmSlug}`)).then(res => res.json()),
+        fetch(encodeURI(`http://${process.env.api}/guilds/guild_logs/${(guildSlug)}@${realmSlug}`)).then(res => res.json())
+    ])
+    return { props: { guild } }
 }
 
 export default GuildPage
---
+++ src/GuildMembers.js
@@ -0,0 +1,100 @@
+import React from "react";
+import TableIcons from "./TableIcons";
+import MaterialTable from "material-table";
+import Link from "./Link";
+
+export default function GuildMembers({data}) {
+    if (!data) return <div>No records available</div>
+    return (
+        <MaterialTable
+            title="Guild Members"
+            icons={TableIcons}
+            columns={[
+                {
+                    field: 'url_id',
+                    title: 'Name',
+                    render: rowData => <Link href={rowData.url_id} color="secondary" underline="hover">{rowData._id}</Link>
+                },
+                {
+                    title: 'Rank',
+                    field: 'rank',
+                    type: 'numeric',
+                    defaultSort: 'asc',
+                    render: rowData => (parseInt(rowData.rank) === 0) ? ('GM') : (rowData.rank)
+                },
+                {
+                    field: 'item_level',
+                    title: 'ilvl',
+                    render: rowData => parseInt(rowData.item_level)
+                },
+                {
+                    field: 'url_a',
+                    title: 'Hash.A',
+                    render: rowData => <Link href={rowData.url_a} color="secondary" underline="hover">{rowData.a}</Link>
+                },
+                {
+                    field: 'url_b',
+                    title: 'Hash.B',
+                    render: rowData => <Link href={rowData.url_b} color="secondary" underline="hover">{rowData.b}</Link>
+                },
+                {
+                    field: 'url_c',
+                    title: 'Hash.C',
+                    render: rowData => <Link href={rowData.url_c} color="secondary" underline="hover">{rowData.c}</Link>
+                },
+                {
+                    title: 'Last Modified',
+                    field: 'lastModified',
+                    cellStyle: {
+                        width: 175,
+                        minWidth: 175
+                    },
+                    render: rowData => new Date(rowData.lastModified).toLocaleString('en-GB')
+                },
+            ]}
+            data={data.map(({_id, name, realm, guild, hash, ilvl, lastModified}) => {
+                let row = {
+                    _id: _id,
+                    url_id: `/character/${realm.name}/${name}`,
+                    rank: `${guild.rank}`,
+                    item_level: 0,
+                    a: 0,
+                    url_a: ``,
+                    b: 0,
+                    url_b: ``,
+                    c: 0,
+                    url_c: ``,
+                    lastModified: lastModified
+                };
+                if (hash) {
+                    if ("a" in hash) {
+                        row.a = hash.a;
+                        row.url_a = `/find/a/${hash.a}`;
+                    }
+                    if ("b" in hash) {
+                        row.b = hash.b;
+                        row.url_b = `/find/b/${hash.b}`;
+                    }
+                    if ("c" in hash) {
+                        row.c = hash.c;
+                        row.url_c = `/find/c/${hash.c}`;
+                    }
+                }
+                if (ilvl) {
+                    if ("eq" in ilvl) {
+                        row.item_level = ilvl.eq;
+                    }
+                }
+                return row
+            })}
+            style={{ backgroundColor: '#ebe7ee', textTransform: "uppercase"}}
+            options={{
+                sorting: true,
+                pageSize: 20,
+                pageSizeOptions: [10,25,50],
+                showTitle: false,
+                headerStyle: {backgroundColor:'#ebe7ee'}
+            }}
+        />
+    )
+}
