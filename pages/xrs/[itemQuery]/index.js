792105793a5c85d0c5b6f190e12ada71d5f468b2
AlexZeDim <AlexZeDim@mail.ru>
2020-08-07 03:09:11 +0300

    XRS WIP

---
+++ pages/xrs/[itemQuery]/index.js
@@ -0,0 +1,141 @@
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import {
+    Container, Grid,
+    Typography, Divider,
+    Avatar, Box
+} from '@material-ui/core';
+import XRSClusterChart from "../../../src/XRSClusterChart";
+import ItemValuations from "../../../src/ItemValuations";
+
+const useStyles = makeStyles(theme => ({
+    table: {
+        minWidth: 400,
+        maxHeight: '760px',
+    },
+    divider: {
+        margin: `${theme.spacing(2)}px auto`,
+    },
+    title: {
+        fontFamily: 'Fira Sans',
+        fontStyle: 'normal',
+        fontDisplay: 'swap',
+        fontWeight: 400,
+    },
+    large: {
+        width: theme.spacing(7),
+        height: theme.spacing(7),
+        marginRight: theme.spacing(2),
+    },
+    cardTitle: {
+        fontSize: '1.1em',
+        fontWeight: 600
+    },
+    titleBlock: {
+        padding: theme.spacing(10, 0, 10),
+    },
+}));
+
+const ItemPage = ({xrs_data}) => {
+
+    let item, chart, quotes, data, _id, icon, name, quality, item_class, item_subclass, ilvl, inventory_type, level, ticker, asset_class;
+
+    if (xrs_data && xrs_data.item) {
+        ({ item, chart } = xrs_data);
+        ({_id, name, quality, icon, item_class, item_subclass, ilvl, inventory_type, level, ticker, asset_class } = item)
+    }
+
+    const classes = useStyles();
+
+    return (
+        <Container maxWidth={false} alignContent="center">
+            {/** TITLE BLOCK */}
+            <Container maxWidth={false} className={classes.titleBlock}>
+                <Grid container direction="column" justify="space-around" alignItems="center" spacing={2}>
+                    <Grid item>
+                        <Box alignItems="center" display="flex" justifyContent="center">
+                            <Avatar alt="Item Icon" variant="rounded" src={icon} className={classes.large} />
+                            <Typography component="h1" variant="h2" color="textPrimary" className={classes.title}>
+                                {(ticker) ? (ticker) : (name["en_GB"])}
+                            </Typography>
+                        </Box>
+                    </Grid>
+                </Grid>
+            </Container>
+
+            {/** CARD BLOCK */}
+            <Container maxWidth="lg">
+                <Grid container spacing={4}>
+                    <Grid item xs={12} sm={6} md={4}>
+                        <Typography gutterBottom variant="overline" display="block" component="h2" className={classes.cardTitle}>
+                            Summary
+                        </Typography>
+                        <Divider light />
+                        <Typography variant="caption" display="block">
+                            ID: {_id}
+                        </Typography>
+                        {(ticker) ? (
+                            <Typography variant="caption" display="block">
+                                Name: {name["en_GB"]}
+                            </Typography>
+                        ) : ('')}
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4}>
+                        <Typography gutterBottom variant="overline" display="block" component="h2" className={classes.cardTitle}>
+                            Class
+                        </Typography>
+                        <Divider light />
+                        <Typography variant="caption" display="block">
+                            {item_class} {item_subclass}
+                        </Typography>
+                        {(asset_class && asset_class.length) ? (
+                            <Typography variant="caption" display="block">
+                                {asset_class.toString().replace(/,/g, ' ')}
+                            </Typography>
+                        ) : ('')}
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4}>
+                        <Typography gutterBottom variant="overline" display="block" component="h2" className={classes.cardTitle}>
+                            Inventory
+                        </Typography>
+                        <Divider light />
+                        <Typography variant="caption" display="block">
+                            {quality} {inventory_type}
+                        </Typography>
+                        {(ilvl && level) ? (
+                            <Typography variant="caption" display="block">
+                                {ilvl} {level}
+                            </Typography>
+                        ) : ("")}
+                    </Grid>
+                </Grid>
+            </Container>
+            {/** MARKET BLOCK */}
+            {(chart) ? (
+                <React.Fragment>
+                    <Divider className={classes.divider} />
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <XRSClusterChart data={chart}/>
+                        </Grid>
+                    </Grid>
+                </React.Fragment>
+            ) : (
+                ''
+            )}
+            <Divider className={classes.divider} />
+                <ItemValuations data={data}/>
+            <Divider className={classes.divider} />
+        </Container>
+    )
+};
+
+
+export async function getServerSideProps({query}) {
+    const { itemQuery } = query;
+    const xrs_data = await fetch(encodeURI(`http://${process.env.api}/items/xrs/${itemQuery}`)).then(res => res.json())
+    return { props: { xrs_data }}
+}
+
+
+export default ItemPage
---
+++ src/XRSClusterChart.js
@@ -0,0 +1,93 @@
+import React from "react";
+import Highcharts from 'highcharts'
+import HighchartsExporting from 'highcharts/modules/exporting'
+import HighchartsReact from 'highcharts-react-official'
+import HC_heatmap from "highcharts/modules/heatmap";
+
+if (typeof Highcharts === 'object') {
+    HC_heatmap(Highcharts);
+    HighchartsExporting(Highcharts)
+}
+
+export default function XRSClusterChart ({data}) {
+    if (!data) return <div>No records available</div>
+
+    const { price_range, realms, dataset } = data;
+
+    return (
+        <HighchartsReact
+            highcharts={Highcharts}
+            constructorType={'chart'}
+            options={{
+                chart: {
+                    type: 'heatmap',
+                    plotBorderWidth: 1,
+                    height: (9 / 16 * 100) + '%',
+                    backgroundColor: 'transparent',
+                    style: {
+                        letterSpacing: 'unset',
+                    }
+                },
+                title: {
+                    text: undefined
+                },
+                xAxis: {
+                    categories: realms.map(({connected_realms}) => `
+                    ${new Date(connected_realms[0].auctions*1000).toLocaleString('ru-RU')}
+                    ${connected_realms.map(r => r.name)}
+                    `),
+                },
+                yAxis:{
+                    categories: price_range,
+                    tickLength: 150,
+                    opposite: false ,
+                    title: null,
+                },
+                colorAxis: {
+                    min: 0,
+                    minColor: '#ebe7ee',
+                    maxColor: '#241c18'
+                },
+                legend: {
+                    align: 'right',
+                    layout: 'vertical',
+                    margin: 0,
+                    verticalAlign: 'middle',
+                    y: 25,
+                    symbolHeight: 350
+                },
+                tooltip: {
+                    formatter: function () {
+                        return `Time & Realms: ${this.series.xAxis.categories[this.point.x]}<br>
+                        Q: ${(this.point.value).toLocaleString('ru-RU')}<br>
+                        P: ${this.series.yAxis.categories[this.point.y]}+<br>
+                        O: ${this.point.orders}<br>
+                        OI: ${(this.point.oi).toLocaleString('ru-RU')}g`
+                    }
+                },
+                series: [{
+                    borderWidth: 0,
+                    clip: false,
+                    data: dataset,
+                    dataLabels: {
+                        enabled: true,
+                        crop: true,
+                        shadow: false,
+                        formatter: function(){
+                            if (this.point.value !== 0) {
+                                return this.point.value.toLocaleString('ru-RU');
+                            }
+                        },
+                        style: {
+                            fontFamily: 'Roboto',
+                            color: 'contrast',
+                            fontSize: '14px',
+                            fontWeight: 'normal',
+                            textOutline: '0px',
+                        }
+                    }
+                }]
+            }}
+        />
+    )
+};
