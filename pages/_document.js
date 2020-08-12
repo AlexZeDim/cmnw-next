e484ad7ca1da1a0f1a7d25883191d257b3dc4d45
AlexZeDim <AlexZeDim@mail.ru>
2020-08-12 03:04:33 +0300

    static => public

--- pages/_document.js
+++ pages/_document.js
@@ -9,11 +9,11 @@ export default class MyDocument extends Document {
             <Html lang="en">
                 <Head>
                     <meta name="theme-color" content={theme.palette.primary.main} />
-                    <link rel="apple-touch-icon" sizes="180x180" href="/public/apple-touch-icon.png"/>
-                    <link rel="icon" type="image/png" sizes="32x32" href="/public/favicon-32x32.png"/>
-                    <link rel="icon" type="image/png" sizes="16x16" href="/public/favicon-16x16.png"/>
-                    <link rel="manifest" href="/public/site.webmanifest"/>
-                    <link rel="mask-icon" href="/public/safari-pinned-tab.svg" color="#5bbad5"/>
+                    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                    <link rel="manifest" href="/site.webmanifest"/>
+                    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
                     <meta name="msapplication-TileColor" content="#da532c"/>
                     <meta name="theme-color" content="#ffffff"/>
                     <link
