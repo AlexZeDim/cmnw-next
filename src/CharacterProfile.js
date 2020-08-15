f3c95427d9e2db10b72861fb61f9c49a28fc6dfa
AlexZeDim <AlexZeDim@mail.ru>
2020-08-15 16:26:38 +0300

    CharacterProfile fix new date

--- src/CharacterProfile.js
+++ src/CharacterProfile.js
@@ -14,26 +14,26 @@ export default function CharacterProfile ({character}) {
     const fieldsToCheck = ["id", "level", "ilvl", "hash", "faction", "gender", "race", "character_class", "spec",  "createdBy", "lastModified"];
 
     return (
-        fieldsToCheck.map(field => {
+        fieldsToCheck.map((field, i) => {
             if (field in character) {
                 if (typeof character[field] === 'object') {
-                    return Object.entries(character[field]).map(([k, v]) => (
+                    return Object.entries(character[field]).map(([k, v], y) => (
                         (field === 'hash') ? (
-                            <Typography variant="caption" display="block" gutterBottom>
+                            <Typography key={i+y} variant="caption" display="block" gutterBottom>
                                 {field} {k}: <Link href={`/find/${k}/${v}`} color="textPrimary" underline="hover">{v}</Link>
                             </Typography>
                         ) : (
-                            <Typography variant="caption" display="block" gutterBottom>
+                            <Typography key={i+y} variant="caption" display="block" gutterBottom>
                                 {humanizeString(field)} {humanizeString(k)}: {v}
                             </Typography>
                         )
                     ))
                 } else {
                     if (field === "lastModified") {
-                        character[field] = new Date(character[field]).toLocaleString('en-GB')
+                        character[field] = (new Date(character[field])).toLocaleString('ru-RU')
                     }
                     return (
-                        <Typography variant="caption" display="block" gutterBottom>
+                        <Typography key={i} variant="caption" display="block" gutterBottom>
                             {humanizeString(field)}: {character[field]}
                         </Typography>
                     )
