8a35cc0fa6256c1c02e815dd25dd7cde28b5f8f9
AlexZeDim <AlexZeDim@mail.ru>
2020-08-16 04:02:56 +0300

    Header remove fade

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { fade, makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Link from "./Link";
 
 const useStyles = makeStyles(theme => ({
