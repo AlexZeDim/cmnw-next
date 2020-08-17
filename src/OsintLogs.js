21935e6e105070c7288d4cd99c09ae69d1288816
AlexZeDim <AlexZeDim@mail.ru>
2020-08-17 05:44:56 +0300

    minor csss fix for OSINT

--- src/OsintLogs.js
+++ src/OsintLogs.js
@@ -10,12 +10,13 @@ export default function OSINT_Logs({data, pageSize = 15}) {
             icons={TableIcons}
             columns={[
                 { title: 'Event', field: 'action', cellStyle: { width: '5%', minWidth: "100px" } },
-                { title: 'Message', field: 'message', cellStyle: { width: '80%', minWidth: "450px" }  },
-                { title: 'After', field: 'after', cellStyle: { width: '10%', minWidth: "175px" },
+                { title: 'Message', field: 'message', cellStyle: { width: '80%', minWidth: "450px" } },
+                {
+                    title: 'After', field: 'after', cellStyle: { width: '10%', minWidth: "175px" },
                     render: rowData => new Date(rowData.after).toLocaleString('en-GB')
-
                 },
-                { title: 'Before', field: 'before', cellStyle: { width: '10%', minWidth: "175px" },
+                {
+                    title: 'Before', field: 'before', cellStyle: { width: '10%', minWidth: "175px" },
                     render: rowData => new Date(rowData.before).toLocaleString('en-GB'),
                     defaultSort: 'desc',
                 }
