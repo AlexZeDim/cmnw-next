b4fcda9359799f3716f9be5b10b17f83863ce1a9
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-08-20 11:05:39 +0300

    contracts header

--- pages/contract/[realmSlug]/[itemSlug]/[tenor]/index.js
+++ pages/contract/[realmSlug]/[itemSlug]/[tenor]/index.js
@@ -7,9 +7,10 @@ import {
     Container, Grid, Typography,
     Divider, ButtonGroup, Button, Avatar, Box,
 } from '@material-ui/core';
-import Router from "next/router";
+import Router, { useRouter } from "next/router";
 import ItemData from "../../../../../src/ItemData";
 import ContractData from "../../../../../src/ContractData";
+import Head from 'next/head'
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -72,50 +73,73 @@ const useStyles = makeStyles(theme => ({
 
 const ContractPage = ({contracts_data}) => {
 
+    const { query } = useRouter()
+
+    let title;
+
     let { item, realm, snapshot, contracts } = contracts_data
+
+    title = `${(item.ticker) ? (item.ticker) : (item.name["en_GB"])}@${(realm.ticker) ? (realm.ticker) : (realm.name)}`;
 
     const classes = useStyles();
 
     return (
-        <Container maxWidth={false}>
-            <Container className={classes.titleBlock}>
-                <Grid container direction="column" justify="space-around" alignItems="center" spacing={2}>
-                    <Grid item xs>
-                        <Box alignItems="center" display="flex" justifyContent="center">
-                            <Avatar alt="Item Icon" variant="rounded" src={item.icon} className={classes.large} />
-                            <Typography variant="h2" className={classes.title} style={{textTransform: 'uppercase'}}>
-                                {(item.ticker) ? (item.ticker) : (item.name["en_GB"])}@{(realm.ticker) ? (realm.ticker) : (realm.name)}
-                            </Typography>
-                        </Box>
-                    </Grid>
-                    <Grid item xs>
-                        <Clock time={realm.auctions*1000}/>
-                    </Grid>
-                    <Grid item xs>
-                        <ButtonGroup color="secondary" aria-label="outlined primary button group">
-                            <Button onClick={() => Router.push(`/item/${realm.connected_realm_id}/${item._id}`)}>Underlying Item</Button>
-                            <Button onClick={() => Router.push(`/xrs/${item._id}`)}>XRS</Button>
-                        </ButtonGroup>
-                    </Grid>
-                </Grid>
-            </Container>
+        <main>
+            <Head>
+                <title>{`CONTRACT ${query.tenor.toUpperCase()}: ${title}`}</title>
+
+                <meta name="description" content="CONTRACTS — historical pricing data management system for latest expansion COMMDTY items"/>
+
+                <meta property="og:type" content="website"/>
+                <meta property="og:url" content="https://conglomerat.group/"/>
+                <meta property="og:title" content={`CONTRACT ${query.tenor.toUpperCase()}: ${title}`}/>
+                <meta property="og:description" content="CONTRACTS — historical pricing data management system for latest expansion COMMDTY items"/>
+
+                <meta property="twitter:card" content="summary_large_image"/>
+                <meta property="og:url" content="https://conglomerat.group/"/>
+                <meta property="twitter:title" content={`CONTRACT ${query.tenor.toUpperCase()}: ${title}`}/>
+                <meta property="twitter:description" content="CONTRACTS — historical pricing data management system for latest expansion COMMDTY items"/>
+            </Head>
             <Container maxWidth={false}>
-                <Divider className={classes.divider} />
-                <Grid container spacing={4}>
-                    <Grid item xs={12} sm={6} md={6} elevation={6}>
-                        <ContractData data={snapshot}/>
+                <Container className={classes.titleBlock}>
+                    <Grid container direction="column" justify="space-around" alignItems="center" spacing={2}>
+                        <Grid item xs>
+                            <Box alignItems="center" display="flex" justifyContent="center">
+                                <Avatar alt="Item Icon" variant="rounded" src={item.icon} className={classes.large} />
+                                <Typography variant="h2" className={classes.title} style={{textTransform: 'uppercase'}}>
+                                    {title}
+                                </Typography>
+                            </Box>
+                        </Grid>
+                        <Grid item xs>
+                            <Clock time={realm.auctions*1000}/>
+                        </Grid>
+                        <Grid item xs>
+                            <ButtonGroup color="secondary" aria-label="outlined primary button group">
+                                <Button onClick={() => Router.push(`/item/${realm.connected_realm_id}/${item._id}`)}>Underlying Item</Button>
+                                <Button onClick={() => Router.push(`/xrs/${item._id}`)}>XRS</Button>
+                            </ButtonGroup>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={12} sm={6} md={6} elevation={6}>
-                        <ItemData data={item}/>
+                </Container>
+                <Container maxWidth={false}>
+                    <Divider className={classes.divider} />
+                    <Grid container spacing={4}>
+                        <Grid item xs={12} sm={6} md={6} elevation={6}>
+                            <ContractData data={snapshot}/>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={6} elevation={6}>
+                            <ItemData data={item}/>
+                        </Grid>
                     </Grid>
-                </Grid>
-                <Divider className={classes.divider} />
-                <LineChart data={contracts}/>
-                <Divider className={classes.divider} />
-                <ContractsTable data={contracts}/>
-                <Divider className={classes.divider} />
+                    <Divider className={classes.divider} />
+                    <LineChart data={contracts}/>
+                    <Divider className={classes.divider} />
+                    <ContractsTable data={contracts}/>
+                    <Divider className={classes.divider} />
+                </Container>
             </Container>
-        </Container>
+        </main>
     )
 };
 
