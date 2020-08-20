5f84330565555cc2268035e046a6081c99aa179f
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-08-20 11:06:42 +0300

    Help pages header

--- pages/help/[slug].js
+++ pages/help/[slug].js
@@ -3,20 +3,38 @@ import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 import ReactMarkdown from "react-markdown/with-html";
-import {Box, CardContent, Container, Grid, Typography} from "@material-ui/core";
-import makeStyles from "@material-ui/core/styles/makeStyles";
+import { Container, Typography } from "@material-ui/core";
+import Head from "next/head";
 
 export default function Post({ content, frontmatter }) {
-
     return (
-        <Container fixed>
-            <Typography variant="body1" component="p">
-                <ReactMarkdown
-                    escapeHtml={false}
-                    source={content}
-                />
-            </Typography>
-        </Container>
+        <main>
+            <Head>
+                <title>{frontmatter.title}</title>
+
+                <meta name="description" content={frontmatter.description}/>
+
+                <meta property="og:type" content="website"/>
+                <meta property="og:url" content="https://conglomerat.group/"/>
+                <meta property="og:title" content={frontmatter.title}/>
+                <meta property="og:description" content={frontmatter.description}/>
+                <meta property="og:image" content="https://conglomerat.group/logo.png"/>
+
+                <meta property="twitter:card" content="summary_large_image"/>
+                <meta property="og:url" content="https://conglomerat.group/"/>
+                <meta property="twitter:title" content={frontmatter.title}/>
+                <meta property="twitter:description" content={frontmatter.description}/>
+                <meta property="og:image" content="https://conglomerat.group/logo.png"/>
+            </Head>
+            <Container fixed>
+                <Typography variant="body1" component="p">
+                    <ReactMarkdown
+                        escapeHtml={false}
+                        source={content}
+                    />
+                </Typography>
+            </Container>
+        </main>
     );
 }
 
--- pages/help/index.js
+++ pages/help/index.js
@@ -8,6 +8,7 @@ import {
     Typography, CardContent,
     CardActions, Button, Box
 } from "@material-ui/core";
+import Head from "next/head";
 
 
 const useStyles = makeStyles(theme => ({
@@ -38,45 +39,64 @@ export default function Help ({ posts }) {
     const classes = useStyles();
 
     return (
-        <Container fixed>
-            <Grid container direction="column" justify="space-around" alignItems="center" spacing={2}>
-                <Box alignItems="center" display="flex" justifyContent="center">
-                    <Container maxWidth={false} className={classes.titleBlock}>
-                        <Typography component="h1" variant="h2" color="textPrimary" className={classes.title}>
-                            HELP
-                        </Typography>
-                    </Container>
-                </Box>
-            </Grid>
-            <Grid
-                container
-                direction="row"
-                justify="flex-start"
-                alignItems="flex-start"
-                spacing={3}
-            >
-            {posts.map(({ slug, frontmatter: { title, description, updatedAt } }, i) => (
-                <Grid key={i} item xs={6} sm={6}>
-                    <Card className={classes.root}>
-                        <CardContent>
-                            <Typography variant="h5" component="h2">
-                                {title}
+        <main>
+            <Head>
+                <title>Conglomerat Help</title>
+
+                <meta name="description" content="Want to report anything? Submit a bug? Grab some bounty? Feel free to go"/>
+
+                <meta property="og:type" content="website"/>
+                <meta property="og:url" content="https://conglomerat.group/"/>
+                <meta property="og:title" content="Conglomerat Help"/>
+                <meta property="og:description" content="Want to report anything? Submit a bug? Grab some bounty? Feel free to go"/>
+                <meta property="og:image" content="https://conglomerat.group/logo.png"/>
+
+                <meta property="twitter:card" content="summary_large_image"/>
+                <meta property="og:url" content="https://conglomerat.group/"/>
+                <meta property="twitter:title" content="Conglomerat Help"/>
+                <meta property="twitter:description" content="Want to report anything? Submit a bug? Grab some bounty? Feel free to go"/>
+                <meta property="og:image" content="https://conglomerat.group/logo.png"/>
+            </Head>
+            <Container fixed>
+                <Grid container direction="column" justify="space-around" alignItems="center" spacing={2}>
+                    <Box alignItems="center" display="flex" justifyContent="center">
+                        <Container maxWidth={false} className={classes.titleBlock}>
+                            <Typography component="h1" variant="h2" color="textPrimary" className={classes.title}>
+                                HELP
                             </Typography>
-                            <Typography color="textSecondary" gutterBottom>
-                                {updatedAt}
-                            </Typography>
-                            <Typography variant="body1" component="p">
-                                {description}
-                            </Typography>
-                        </CardContent>
-                        <CardActions>
-                            <Button onClick={() => Router.push(`/help/${slug}`)} size="small">Learn More</Button>
-                        </CardActions>
-                    </Card>
+                        </Container>
+                    </Box>
                 </Grid>
-            ))}
-            </Grid>
-        </Container>
+                <Grid
+                    container
+                    direction="row"
+                    justify="flex-start"
+                    alignItems="flex-start"
+                    spacing={3}
+                >
+                {posts.map(({ slug, frontmatter: { title, description, updatedAt } }, i) => (
+                    <Grid key={i} item xs={6} sm={6}>
+                        <Card className={classes.root}>
+                            <CardContent>
+                                <Typography variant="h5" component="h2">
+                                    {title}
+                                </Typography>
+                                <Typography color="textSecondary" gutterBottom>
+                                    {updatedAt}
+                                </Typography>
+                                <Typography variant="body1" component="p">
+                                    {description}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button onClick={() => Router.push(`/help/${slug}`)} size="small">Learn More</Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                ))}
+                </Grid>
+            </Container>
+        </main>
     );
 }
 
