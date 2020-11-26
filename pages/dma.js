b8f9a6acd3b8cfdab7607fd2d27933fdc430b87b
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-11-26 09:07:58 +0300

    fix minor typo

--- pages/dma.js
+++ pages/dma.js
@@ -181,7 +181,7 @@ export default function DMA() {
           </Formik>
         </Container>
         <Typography variant="overline" align="center" style={{textTransform: 'uppercase'}}>
-          Any language, every item, many realms,much evaluation, very wow!
+          Any language, every item, many realms, much evaluation, very wow!
         </Typography>
       </Grid>
     </main>
