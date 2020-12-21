47deee68bc1828bcf7a9aa46a1ba6991529fe8f8
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2020-12-21 05:37:37 +0300

    character: guild rank

--- pages/character/[id].js
+++ pages/character/[id].js
@@ -86,12 +86,7 @@ function CharacterPage({character}) {
                 {(guild && realm.slug && guild.slug) ? (
                   <Typography variant="h4" component="h4" color="textPrimary" className={classes.title}>
                     #<Link href={`/guild/${guild.slug}@${realm.slug}`} color="textPrimary" underline="hover">{guild.name}</Link>
-                    {() => {
-                      if ('rank' in guild) {
-                        if (parseInt(guild.rank) === 0) return (` // GM`)
-                        if (parseInt(guild.rank) > 0 && parseInt(guild.rank) < 13) return (` // R${guild.rank}`)
-                      }
-                    }}
+                    {(guild.rank || guild.rank !== null) ? ((parseInt(guild.rank) === 0) ? (` // GM`) : (` // R${guild.rank}`)) : ('')}
                   </Typography>
                 ) : ('')}
                 <Typography variant="h4" component="h4" color="textPrimary" className={classes.title}>
