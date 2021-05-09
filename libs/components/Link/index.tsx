c639a2ddaa306fd832c5088b66c7a28db46fff88
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-05-09 18:45:22 +0300

    feat(components): Link

---
+++ libs/components/Link/index.tsx
@@ -0,0 +1,73 @@
+import React, { ForwardedRef } from 'react';
+import NextLink from 'next/link';
+import PropTypes from 'prop-types';
+import { useRouter } from 'next/router';
+import clsx from 'clsx';
+import MuiLink from '@material-ui/core/Link';
+
+type Props = {
+  as: string,
+  href: string,
+  prefetch: boolean
+}
+
+const NextComposed: React.FC<Props> = React.forwardRef(function NextComposed(props, ref: ForwardedRef<HTMLAnchorElement>) {
+  const { as, href, prefetch, ...other } = props;
+
+  return (
+    <NextLink href={href} prefetch={prefetch} as={as}>
+      <a ref={ref} {...other} />
+    </NextLink>
+  );
+});
+
+/* TODO
+  type Link = {
+  activeClassName: string,
+  as: string,
+  className: string,
+  href: string,
+  innerRef: any, //PropTypes.oneOfType([PropTypes.func, PropTypes.object])
+  naked: boolean,
+  onClick: React.MouseEventHandler<HTMLButtonElement>,
+  prefetch: boolean,
+}
+*/
+
+/**
+ * A styled version of the Next.js Link component
+ * https://nextjs.org/docs/#with-link
+ */
+function Link(props) {
+  const {
+    activeClassName = 'active',
+    className: classNameProps,
+    innerRef,
+    naked,
+    ...other
+  } = props;
+  const router = useRouter();
+
+  const className = clsx(classNameProps, {
+    [activeClassName]: router.pathname === props.href && activeClassName,
+  });
+
+  if (naked) {
+    return <NextComposed className={className} ref={innerRef} {...other} />;
+  }
+
+  return <MuiLink component={NextComposed} className={className} ref={innerRef} {...other} />;
+}
+
+Link.propTypes = {
+  activeClassName: PropTypes.string,
+  as: PropTypes.string,
+  className: PropTypes.string,
+  href: PropTypes.string,
+  innerRef: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
+  naked: PropTypes.bool,
+  onClick: PropTypes.func,
+  prefetch: PropTypes.bool,
+};
+
+export default React.forwardRef((props, ref: ForwardedRef<string>) => <Link {...props} innerRef={ref}/>);
---
+++ libs/constants/header.ts
@@ -0,0 +1,22 @@
+export const HEADER = [
+  {
+    label: "Characters & Guilds",
+    href: "/osint",
+  },
+  {
+    label: "Auctions & Valuations",
+    href: "/dma",
+  },
+  {
+    label: "Discord Bot",
+    href: "/discord",
+  },
+  {
+    label: "Help",
+    href: "/help",
+  },
+  {
+    label: "Who we are",
+    href: "/who-we-are",
+  },
+];
