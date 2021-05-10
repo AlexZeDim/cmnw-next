b1c7c74b149dae9a1eb14c5ec2ab0e1560b6808b
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-05-10 11:59:00 +0300

    refactor(components): type Link

---
+++ libs/types/components/linkMui.ts
@@ -0,0 +1,10 @@
+export type linkMui = {
+  activeClassName: string,
+  as: string,
+  className: string,
+  href: string,
+  innerRef: (any) => any | Record<string, any>,
+  naked: boolean,
+  onClick: () => void,
+  prefetch: boolean,
+};
