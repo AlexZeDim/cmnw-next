1aaefe156417684ceaa6a002a1a919d16e8eba6c
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-05-15 18:50:48 +0300

    feat(pages): index page

--- libs/constants/pages.ts
+++ libs/constants/pages.ts
@@ -1,14 +1,19 @@
 export const CHARACTER_PAGE = {
   basename: '/character',
-  description: 'CHARACTER — Provides a requested information about certain game character across OSINT-DB'
+  description: 'CHARACTER — Provides a requested information about certain game character across OSINT-DB',
 }
 
 export const HASH_PAGE = {
   basename: '/hash',
-  description: 'HASH — all available hash matches for dynamic hash value. Don\'t bookmark this page!'
+  description: 'HASH — all available hash matches for dynamic hash value. Don\'t bookmark this page!',
 }
 
 export const GUILD_PAGE = {
   basename: '/guild',
-  description: 'GUILD — all available information about selected guild, like members and OSINT logs.'
+  description: 'GUILD — all available information about selected guild, like members and OSINT logs.',
+}
+
+export const INDEX_PAGE = {
+  basename: './',
+  description: 'World of Warcraft: Intellegence always wins.',
 }
