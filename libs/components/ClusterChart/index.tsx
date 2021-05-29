7a6a45e48a702bd04683a8ef74476cf661cc13b1
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-05-29 15:49:06 +0300

    feat(components): clusterChart useSWR

--- libs/components/ClusterChart/index.tsx
+++ libs/components/ClusterChart/index.tsx
@@ -1,98 +1,30 @@
-import React, { FC, Fragment, useEffect, useState } from 'react';
-import Highcharts from 'highcharts'
-import HighchartsExporting from 'highcharts/modules/exporting'
-import HighchartsReact from 'highcharts-react-official'
-import HC_heatmap from 'highcharts/modules/heatmap';
+import React, { FC, Fragment } from 'react';
+import useSWR from 'swr';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+import HighchartsExporting from 'highcharts/modules/exporting';
+import Heatmap from 'highcharts/modules/heatmap';
 import { clusterChart } from '../../types/components/clusterChart';
+import { LinearProgress } from '@material-ui/core';
 import { chartResponse } from '../../types/components/chartResponse';
 
-console.log(typeof Highcharts);
-
 if (typeof Highcharts === 'object') {
-  HC_heatmap(Highcharts);
+  Heatmap(Highcharts);
   HighchartsExporting(Highcharts)
 }
 
 const ClusterChart: FC<clusterChart> = ({ _id }) => {
 
-  const [options, setOptions] = useState({
-    chart: {
-      type: 'heatmap',
-      plotBorderWidth: 1,
-      height: (9 / 16 * 100) + '%',
-      backgroundColor: 'transparent',
-      style: {
-        letterSpacing: 'unset',
-      }
-    },
-    title: {
-      text: undefined
-    },
-    xAxis: {
-      categories: [],
-    },
-    yAxis: {
-      categories: [],
-      labels: {
-        formatter: function () {
-          return this.value.toFixed(2);
-        }
-      },
-      tickLength: 150,
-      opposite: false,
-      title: null,
-    },
-    colorAxis: {
-      min: 0,
-      minColor: 'rgba(167,167,167,0.1)',
-      maxColor: 'rgba(167,167,167,1)'
-    },
-    legend: {
-      align: 'right',
-      layout: 'vertical',
-      margin: 0,
-      verticalAlign: 'middle',
-      y: 25,
-      symbolHeight: 350
-    },
-    tooltip: {
-      formatter: function () {
-        return `T: ${this.series.xAxis.categories[this.point.x]}<br>
-                        Q: ${(this.point.value).toLocaleString('ru-RU')}<br>
-                        P: ${this.series.yAxis.categories[this.point.y]}+<br>
-                        O: ${this.point.orders}<br>
-                        OI: ${parseInt(this.point.oi).toLocaleString('ru-RU')}`
-      }
-    },
-    series: [{
-      borderWidth: 0,
-      clip: false,
-      data: [],
-      dataLabels: {
-        enabled: true,
-        crop: true,
-        shadow: true,
-        formatter: function () {
-          if (this.point.value !== 0) {
-            return this.point.value.toLocaleString('ru-RU');
-          }
-        },
-        style: {
-          fontFamily: 'Roboto',
-          color: '#242424',
-          fontSize: '14px',
-          fontWeight: 'normal',
-          textOutline: '0px',
-        }
-      }
-    }]
-  });
+  const { data, error } = useSWR<chartResponse>(`http://localhost:8000/api/dma/item/chart?_id=${_id}`, (url) => fetch(url).then(r => r.json()));
 
-  useEffect(() => {
-    fetch(`http://localhost:8000/api/dma/item/chart?_id=${_id}`)
-      .then(response => response.json())
-      .then(({ xAxis, yAxis, dataset }: chartResponse) => {
-        setOptions({
+  if (error) return <div>failed to load</div>
+  if (!data) return <LinearProgress />
+  return (
+    <Fragment>
+      <HighchartsReact
+        highcharts={Highcharts}
+        constructorType={'chart'}
+        options={{
           chart: {
             type: 'heatmap',
             plotBorderWidth: 1,
@@ -106,15 +38,16 @@ const ClusterChart: FC<clusterChart> = ({ _id }) => {
             text: undefined
           },
           xAxis: {
-            categories: xAxis,
-          },
-          yAxis: {
-            categories: yAxis,
+            categories: data.xAxis,
             labels: {
               formatter: function () {
-                return this.value.toFixed(2);
+                if (typeof this.value === 'number') return new Date(this.value).toLocaleString('ru-RU');
+                return this.value;
               }
-            },
+            }
+          },
+          yAxis: {
+            categories: data.yAxis,
             tickLength: 150,
             opposite: false,
             title: null,
@@ -134,17 +67,13 @@ const ClusterChart: FC<clusterChart> = ({ _id }) => {
           },
           tooltip: {
             formatter: function () {
-              return `T: ${this.series.xAxis.categories[this.point.x]}<br>
-                        Q: ${(this.point.value).toLocaleString('ru-RU')}<br>
-                        P: ${this.series.yAxis.categories[this.point.y]}+<br>
-                        O: ${this.point.orders}<br>
-                        OI: ${parseInt(this.point.oi).toLocaleString('ru-RU')}`
+              return `${typeof this.series.xAxis.categories[this.point.x] === 'number' ? new Date(this.series.xAxis.categories[this.point.x]).toLocaleString('ru-RU'): this.series.xAxis.categories[this.point.x]}<br>Quantity: ${(this.point.value).toLocaleString('ru-RU')}<br>Price: ${this.series.yAxis.categories[this.point.y]}<br>Orders: ${this.point.orders}<br>Open Interest: ${parseInt(this.point.oi).toLocaleString('ru-RU')}`
             }
           },
           series: [{
             borderWidth: 0,
             clip: false,
-            data: dataset,
+            data: data.dataset,
             dataLabels: {
               enabled: true,
               crop: true,
@@ -155,24 +84,15 @@ const ClusterChart: FC<clusterChart> = ({ _id }) => {
                 }
               },
               style: {
-                fontFamily: 'Roboto',
+                fontFamily: 'Fira Sans',
                 color: '#242424',
-                fontSize: '14px',
+                fontSize: '16px',
                 fontWeight: 'normal',
                 textOutline: '0px',
               }
             }
           }]
-        });
-      });
-  }, []);
-
-  return (
-    <Fragment>
-      <HighchartsReact
-        highcharts={Highcharts}
-        constructorType={'chart'}
-        options={options}
+        }}
       />
     </Fragment>
   )
