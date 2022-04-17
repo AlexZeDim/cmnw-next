eecbadf21d1622f1bda95c148a4cf6337bb9e3fc
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2022-04-17 19:48:51 +0300

    fix(components): fix OG headers

--- libs/components/MetaHead/index.tsx
+++ libs/components/MetaHead/index.tsx
@@ -11,13 +11,13 @@ const MetaHead: React.FC<metaHead> = ({ title, description, image, wowhead = fal
       <meta name="description" content={description}/>
 
       <meta property="og:type" content="website"/>
-      <meta property="og:url" content={DOMAIN}/>
+      <meta property="og:url" content={DOMAINS.domain}/>
       <meta property="og:title" content={title}/>
       <meta property="og:description" content={description}/>
       <meta property="og:image" content={image}/>
 
       <meta property="twitter:card" content="summary_large_image"/>
-      <meta property="og:url" content={DOMAIN}/>
+      <meta property="og:url" content={DOMAINS.domain}/>
       <meta property="twitter:title" content={title}/>
       <meta property="twitter:description" content={description}/>
       <meta property="twitter:image" content={image}/>
