5ba93127ce30de47b0f3db5eb6224260b11738c0
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2022-10-14 18:12:20 +0300

    refactor(components) search form with v5

--- libs/components/SearchForm/index.tsx
+++ libs/components/SearchForm/index.tsx
@@ -1,209 +1,41 @@
-import { object, string, array } from 'yup';
-import { useRouter } from 'next/router'
-import MuiTextField from '@material-ui/core/TextField';
-import { Field, Form, Formik } from 'formik';
-import { TextField } from 'formik-material-ui';
-import { Autocomplete } from 'formik-material-ui-lab';
+import  { useRouter } from 'next/router'
 import React, { FC } from 'react';
-import { Button, Grid, makeStyles, MenuItem } from '@material-ui/core';
 import { COMMANDS, REALMS, HASH } from '../../constants';
 import AtSign from '../AtSign';
 import ArrowForwardOutlinedIcon from '@material-ui/icons/ArrowForwardOutlined';
-import { Commands, HashType, initialValuesSearch, searchValidation } from '../../types';
+import { Commands, searchValidation } from '../../types';
 import { initialValues } from '../../utils';
+import { useFormik } from 'formik';
+import { TextField, Autocomplete, Button } from '@mui/material';
 
-const useStyles = makeStyles(() => ({
-  item: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    minWidth: '20em',
-  },
-  button: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  }
-}));
+
 
 export const SearchForm: FC = () => {
-  const classes = useStyles();
   const router = useRouter();
 
+  const formik = useFormik({
+    initialValues: REALMS[0],
+    validationSchema: searchValidation,
+    onSubmit: (values) => {
+      console.log(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
-    <Formik
-      initialValues={initialValues()}
-      validationSchema={searchValidation}
-      onSubmit={
-        async (values, formikHelpers) => {
-          console.log(values, formikHelpers);
-          formikHelpers.setSubmitting(true);
-          // await submitSearchForm(values);
-        }
-      }
-    >
-      {({ values, touched, errors}) => (
-        <Form>
-          <Grid container spacing={5} alignItems={'center'} justifyContent={"center"}>
-            <Grid item xs={12} md={3}>
-              <div className={classes.item}>
-                <Field
-                  component={TextField}
-                  type="text"
-                  name="command"
-                  label="Command"
-                  select
-                  variant="outlined"
-                  fullWidth
-                  InputLabelProps={{
-                    shrink: true,
-                  }}
-                >
-                  {COMMANDS.map((option) => (
-                    <MenuItem key={option.value} value={option.value}>
-                      {option.label}
-                    </MenuItem>
-                  ))}
-                </Field>
-              </div>
-            </Grid>
-            {values.command === Commands.commdty && (
-              <React.Fragment>
-                <Grid item xs={12} md={7}>
-                  <div className={classes.item}>
-                    <Field
-                      component={TextField}
-                      name="commdty"
-                      type="text"
-                      label="Commdty"
-                      variant="outlined"
-                      fullWidth
-                    />
-                  </div>
-                </Grid>
-              </React.Fragment>
-            )}
-            {values.command === Commands.characters && (
-              <React.Fragment>
-                <Grid item xs={12} md={3}>
-                  <div className={classes.item}>
-                    <Field
-                      component={TextField}
-                      name="character"
-                      type="text"
-                      label="Character"
-                      variant="outlined"
-                      fullWidth
-                    />
-                  </div>
-                </Grid>
-                <AtSign/>
-                <Grid item xs={12} md={3}>
-                  <div className={classes.item}>
-                    <Field
-                      name="realm"
-                      component={Autocomplete}
-                      fullWidth
-                      options={REALMS}
-                      getOptionLabel={(option) => option.label}
-                      renderInput={(params) => (
-                        <MuiTextField
-                          {...params}
-                          error={touched['realm'] && !!errors['realm']}
-                          label="Realm"
-                          variant="outlined"
-                        />
-                      )}
-                    />
-                  </div>
-                </Grid>
-              </React.Fragment>
-            )}
-            {values.command === Commands.guilds && (
-              <React.Fragment>
-                <Grid item xs={12} md={3}>
-                  <div className={classes.item}>
-                    <Field
-                      component={TextField}
-                      name="guild"
-                      type="text"
-                      label="Guild"
-                      variant="outlined"
-                      fullWidth
-                    />
-                  </div>
-                </Grid>
-                <AtSign/>
-                <Grid item xs={12} md={3}>
-                  <div className={classes.item}>
-                    <Field
-                      name="realm"
-                      component={Autocomplete}
-                      fullWidth
-                      options={REALMS}
-                      getOptionLabel={(option) => option.label}
-                      renderInput={(params) => (
-                        <MuiTextField
-                          {...params}
-                          error={touched['realm'] && !!errors['realm']}
-                          label="Realm"
-                          variant="outlined"
-                        />
-                      )}
-                    />
-                  </div>
-                </Grid>
-              </React.Fragment>
-            )}
-            {values.command === Commands.hash && (
-              <React.Fragment>
-                <Grid item xs={12} md={3}>
-                  <div className={classes.item}>
-                    <Field
-                      component={TextField}
-                      fullWidth
-                      type="text"
-                      name="type"
-                      label="Type"
-                      select
-                      variant="outlined"
-                      InputLabelProps={{
-                        shrink: true,
-                      }}
-                    >
-                      {HASH.map((option) => (
-                        <MenuItem key={option.value} value={option.value}>
-                          {option.label}
-                        </MenuItem>
-                      ))}
-                    </Field>
-                  </div>
-                </Grid>
-                <AtSign/>
-                <Grid item xs={12} md={3}>
-                  <div className={classes.item}>
-                    <Field
-                      component={TextField}
-                      name="hash"
-                      type="text"
-                      label="Hash"
-                      variant="outlined"
-                      fullWidth
-                    />
-                  </div>
-                </Grid>
-              </React.Fragment>
-            )}
-            <Grid item xs={12} md={1}>
-              <div className={classes.button}>
-                <Button type="submit" variant="outlined" color="secondary" size="large">
-                  <ArrowForwardOutlinedIcon/>
-                </Button>
-              </div>
-            </Grid>
-          </Grid>
-        </Form>
-      )}
-    </Formik>
+    <div>
+      <form onSubmit={formik.handleSubmit}>
+        <Autocomplete
+          id="grouped-demo"
+          options={REALMS.sort((a, b) => -b.label.localeCompare(a.label))}
+          groupBy={(option) => option.label}
+          getOptionLabel={(option) => option.label}
+          sx={{ width: 300 }}
+          renderInput={(params) => <TextField {...params} label="REALM" />}
+        />
+        <Button color="primary" variant="contained" fullWidth type="submit">
+          Submit
+        </Button>
+      </form>
+    </div>
   )
 }
