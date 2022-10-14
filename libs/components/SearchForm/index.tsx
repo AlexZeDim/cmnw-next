65929f062e09459120ab2d787069b8206f900614
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2022-10-14 18:16:15 +0300

    refactor(components) search form with v5 via formik

--- libs/components/SearchForm/index.tsx
+++ libs/components/SearchForm/index.tsx
@@ -5,7 +5,7 @@ import AtSign from '../AtSign';
 import ArrowForwardOutlinedIcon from '@material-ui/icons/ArrowForwardOutlined';
 import { Commands, searchValidation } from '../../types';
 import { initialValues } from '../../utils';
-import { useFormik } from 'formik';
+import { Formik, Field, Form } from 'formik';
 import { TextField, Autocomplete, Button } from '@mui/material';
 
 
@@ -13,29 +13,31 @@ import { TextField, Autocomplete, Button } from '@mui/material';
 export const SearchForm: FC = () => {
   const router = useRouter();
 
-  const formik = useFormik({
-    initialValues: REALMS[0],
-    validationSchema: searchValidation,
-    onSubmit: (values) => {
-      console.log(JSON.stringify(values, null, 2));
-    },
-  });
-
   return (
     <div>
-      <form onSubmit={formik.handleSubmit}>
-        <Autocomplete
-          id="grouped-demo"
-          options={REALMS.sort((a, b) => -b.label.localeCompare(a.label))}
-          groupBy={(option) => option.label}
-          getOptionLabel={(option) => option.label}
-          sx={{ width: 300 }}
-          renderInput={(params) => <TextField {...params} label="REALM" />}
-        />
-        <Button color="primary" variant="contained" fullWidth type="submit">
-          Submit
-        </Button>
-      </form>
+      <Formik
+        initialValues={initialValues()}
+        validationSchema={searchValidation}
+        onSubmit={async (values) => {
+          alert(JSON.stringify(values, null, 2));
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <Autocomplete
+              id="realm"
+              options={REALMS.sort((a, b) => -b.label.localeCompare(a.label))}
+              groupBy={(option) => option.label}
+              getOptionLabel={(option) => option.label}
+              sx={{ width: 300 }}
+              renderInput={(params) => <TextField {...params} label="REALM" />}
+            />
+            <Button color="primary" variant="contained" fullWidth type="submit">
+              Submit
+            </Button>
+          </Form>
+        )}
+      </Formik>
     </div>
   )
 }
