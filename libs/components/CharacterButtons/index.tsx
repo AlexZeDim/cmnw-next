6ae6cee98e48f9169e148552cc7d069241c14d00
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2022-10-19 13:43:05 +0300

    refactor(components): characterButtons v5

--- libs/components/CharacterButtons/index.tsx
+++ libs/components/CharacterButtons/index.tsx
@@ -1,41 +1,42 @@
-import { Avatar, Grid, makeStyles } from '@material-ui/core';
-import { characterButtons } from '../../types/components';
+import { Avatar, Grid } from '@mui/material';
+import { characterButtons } from '../../types';
 import React, { FC, Fragment } from 'react';
 import Link from '../Link';
 import { DOMAINS } from '../../constants';
+import { theme } from '../../styles/theme';
 
-const useStyles = makeStyles((theme) => ({
+const styleCss = {
   large: {
     width: theme.spacing(8),
     height: theme.spacing(8),
     margin: theme.spacing(2),
   }
-}));
+};
+
 
 const CharacterButtons: FC<characterButtons> = ({ name, realm }) => {
-  const classes = useStyles();
   return (
     <Fragment>
       <Grid
         container
-        direction="column"
-        justify="flex-start"
-        alignItems="center"
+        direction='row'
+        alignItems={'center'}
+        justifyContent={'center'}
       >
         <Link href={`${DOMAINS.warcraftLogs}/character/eu/${realm}/${name}`} prefetch={false}>
-          <Avatar variant="square" alt="WarcraftLogs" src={'/static/wcl.svg'} className={classes.large}/>
+          <Avatar variant="square" alt="WarcraftLogs" src={'/static/wcl.svg'} sx={styleCss.large}/>
         </Link>
         <Link href={`${DOMAINS.raiderIo}/characters/eu/${realm}/${name}`} prefetch={false}>
-          <Avatar variant="square" alt="RaiderIO" src={'/static/rio.svg'} className={classes.large}/>
+          <Avatar variant="square" alt="RaiderIO" src={'/static/rio.svg'} sx={styleCss.large}/>
         </Link>
         <Link href={`${DOMAINS.wowProgress}/character/eu/${realm}/${name}`} prefetch={false}>
-          <Avatar variant="square" alt="WoWProgress" src={'/static/wp.svg'} className={classes.large} />
+          <Avatar variant="square" alt="WoWProgress" src={'/static/wp.svg'} sx={styleCss.large} />
         </Link>
         <Link href={`${DOMAINS.battleNet}/en-gb/character/eu/${realm}/${name}`} prefetch={false}>
-          <Avatar variant="square" alt="BattleNet" src={'/static/armory.svg'} className={classes.large}/>
+          <Avatar variant="square" alt="BattleNet" src={'/static/armory.svg'} sx={styleCss.large}/>
         </Link>
         <Link href={`${DOMAINS.checkPvp}/eu/${realm}/${name}`} prefetch={false}>
-          <Avatar variant="square" alt="Check PvP" src={'/static/check_pvp.svg'} className={classes.large} />
+          <Avatar variant="square" alt="Check PvP" src={'/static/check_pvp.svg'} sx={styleCss.large} />
         </Link>
       </Grid>
     </Fragment>
