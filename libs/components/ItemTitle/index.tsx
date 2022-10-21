ace1b234dc27f62297eeb6eba4be7b30d3295cc2
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2022-10-21 11:35:43 +0300

    refactor(components): ItemTitle v5

--- libs/components/ItemTitle/index.tsx
+++ libs/components/ItemTitle/index.tsx
@@ -1,9 +1,10 @@
-import React, { FC, Fragment } from 'react';
-import { Avatar, Box, Divider, Grid, makeStyles, Typography } from '@material-ui/core';
-import { itemTitle } from '../../types/components';
+import { FC, Fragment } from 'react';
+import { Avatar, Box, Divider, Grid, Typography } from '@mui/material';
+import { itemTitle } from '../../types';
 import { generateItemBackground } from '../../utils';
+import { theme } from '../../styles';
 
-const useStyles = makeStyles((theme) => ({
+const styleCss = {
   root: {
     margin: theme.spacing(2),
     padding: theme.spacing(4),
@@ -40,29 +41,28 @@ const useStyles = makeStyles((theme) => ({
   divider: {
     marginBottom: '15px',
   },
-}));
+};
 
 const ItemTitle: FC<itemTitle> = ({ itemTitle, realmTitle, quality, asset_class, icon}) => {
-  const classes = useStyles();
   const backgroundRoot = generateItemBackground({ quality, asset_class });
   const backgroundTitle = generateItemBackground({ asset_class });
   const borderColor = { borderColor: backgroundTitle.backgroundColor };
   return (
     <Fragment>
-      <div className={classes.root} style={backgroundRoot}>
-        <div className={classes.title} style={{ ...backgroundRoot, ...borderColor}}>
+      <Box sx={styleCss.root} style={backgroundRoot}>
+        <Box sx={styleCss.title} style={{ ...backgroundRoot, ...borderColor}}>
           <Grid alignItems="center" justifyContent="flex-start">
-            <Avatar alt="Item Icon" variant="rounded" src={icon} className={classes.large}/>
-            <Typography variant="h1" component="h1" color="textPrimary" className={classes.item}>
+            <Avatar alt="Item Icon" variant="rounded" src={icon} sx={styleCss.large}/>
+            <Typography variant="h1" component="h1" color="textPrimary" sx={styleCss.item}>
               {itemTitle}
             </Typography>
           </Grid>
-          <Divider className={classes.divider} style={{ background: backgroundTitle.backgroundColor }}/>
-          <Typography variant="h4" component="h3" color="textPrimary" className={classes.realm}>
+          <Divider sx={styleCss.divider} style={{ background: backgroundTitle.backgroundColor }}/>
+          <Typography variant="h4" component="h3" color="textPrimary" sx={styleCss.realm}>
             {realmTitle}
           </Typography>
-        </div>
-      </div>
+        </Box>
+      </Box>
     </Fragment>
   )
 }
