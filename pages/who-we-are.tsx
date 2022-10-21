19ce3fb7928ba82b7a2b852ca7d5d9e3b9c2bfd3
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2022-10-21 11:57:46 +0300

    refactor(pages): who-we-are v5 <meta> => Box

--- pages/who-we-are.tsx
+++ pages/who-we-are.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import MetaHead from '../libs/components/MetaHead';
-import { WHO_WE_ARE, CONTRIBUTORS } from '../libs/constants';
-import { Container, Divider, Grid, makeStyles, Typography } from '@material-ui/core';
+import { Container, Divider, Grid, Typography, Box } from '@mui/material';
+import { WHO_WE_ARE, CONTRIBUTORS } from '../libs';
 import ContributionStar from '../libs/components/ContributionStar';
+import MetaHead from '../libs/components/MetaHead';
+import { theme } from '../libs/styles';
 
-const useStyles = makeStyles(theme => ({
+const styleCss = {
   main: {
     marginTop: '65px',
     minHeight: '90vh',
@@ -16,36 +16,37 @@ const useStyles = makeStyles(theme => ({
     margin: theme.spacing(4),
     textTransform: 'uppercase',
   }
-}));
+};
 
 const WhoWeAre = () => {
-  const classes = useStyles();
   return (
-    <main className={classes.main}>
-      <MetaHead
-        title={'The Conglomerat'}
-        description={WHO_WE_ARE.description}
-        wowhead={false}/>
-      <Container maxWidth={false} className={classes.divider}>
-        <Divider/>
-        <Typography variant="h2" component="h1" align="center" className={classes.memory} gutterBottom>
-          Great Many Thanks
-        </Typography>
-        <Divider/>
-        <Grid container>
-          {CONTRIBUTORS.map((contributor, i) => (
-            <Grid item xs={3} key={i}>
-              <ContributionStar
-                character={contributor.character}
-                name={contributor.name}
-                discord={contributor.discord}
-                twitter={contributor.twitter}
-                github={contributor.github}
-              />
-            </Grid>
-          ))}
-        </Grid>
-      </Container>
+    <main>
+      <Box sx={styleCss.main}>
+        <MetaHead
+          title={'The Conglomerat'}
+          description={WHO_WE_ARE.description}
+          wowhead={false}/>
+        <Container maxWidth={false} sx={styleCss.divider}>
+          <Divider/>
+          <Typography variant="h2" component="h1" align="center" sx={styleCss.memory} gutterBottom>
+            Great Many Thanks
+          </Typography>
+          <Divider/>
+          <Grid container>
+            {CONTRIBUTORS.map((contributor, i) => (
+              <Grid item xs={3} key={i}>
+                <ContributionStar
+                  character={contributor.character}
+                  name={contributor.name}
+                  discord={contributor.discord}
+                  twitter={contributor.twitter}
+                  github={contributor.github}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </Box>
     </main>
   )
 }
