cd0064029216b6986efc5dc73333d633453db82e
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2022-10-21 11:25:35 +0300

    refactor(components): HashTitle v5

--- libs/components/HashTitle/index.tsx
+++ libs/components/HashTitle/index.tsx
@@ -1,9 +1,10 @@
-import React, { FC, Fragment, useState } from 'react';
+import { FC, Fragment, useState, MouseEvent } from 'react';
 import { hashTitle } from '../../types/components/hashTitle';
-import { Divider, makeStyles, Popover, Typography } from '@material-ui/core';
+import { Divider, Popover, Typography, Box } from '@mui/material';
 import { generateFactionBackground } from '../../utils';
+import { theme } from '../../styles';
 
-const useStyles = makeStyles((theme) => ({
+const styleCss = {
   root: {
     margin: theme.spacing(2),
     padding: theme.spacing(4),
@@ -42,7 +43,7 @@ const useStyles = makeStyles((theme) => ({
     background: theme.palette.primary.main,
     color: '#f5f2f2',
   },
-}));
+};
 
 const HashTitle: FC<hashTitle> = ({ id }) => {
   const [copyStatus, setCopyStatus] = useState<string | null>(null);
@@ -51,7 +52,7 @@ const HashTitle: FC<hashTitle> = ({ id }) => {
   const [type, query] = id.split('@');
   const hash = query.replace(/(.{4})/g, '$1 ');
 
-  const handleClick = async (textBuffer: string, event: React.MouseEvent<HTMLElement, MouseEvent>) => {
+  const handleClick = async (textBuffer: string, event: MouseEvent<HTMLElement, MouseEvent>) => {
     try {
       await navigator.clipboard.writeText(textBuffer);
       setCopyStatus(`Hash ${id} has been coped!`);
@@ -68,28 +69,24 @@ const HashTitle: FC<hashTitle> = ({ id }) => {
   const open = Boolean(anchorEl);
   const pid = open ? 'text-buffer' : undefined;
 
-  const classes = useStyles();
   const background = generateFactionBackground();
   return (
     <Fragment>
-      <div className={classes.root} style={background}>
-        <div className={classes.title} style={background}>
+      <Box sx={styleCss.root} style={background}>
+        <Box sx={styleCss.title} style={background}>
           <Typography
             onClick={(e) => handleClick(id, e)}
             onMouseLeave={handlePopoverClose}
             variant="h1"
             component="h1"
             color="textPrimary"
-            className={classes.hash}
+            sx={styleCss.hash}
           >
             {hash}
           </Typography>
           <Popover
             id={pid}
-            className={classes.popover}
-            classes={{
-              paper: classes.paper,
-            }}
+            sx={styleCss.popover}
             open={open}
             anchorOrigin={{
               vertical: 'bottom',
@@ -104,17 +101,17 @@ const HashTitle: FC<hashTitle> = ({ id }) => {
           >
             <Typography>{copyStatus}</Typography>
           </Popover>
-          <Divider className={classes.divider}/>
+          <Divider sx={styleCss.divider}/>
           <Typography
             variant="h2"
             component="h2"
             color="textPrimary"
-            className={classes.type}
+            sx={styleCss.type}
           >
             Type {type}
           </Typography>
-        </div>
-      </div>
+        </Box>
+      </Box>
     </Fragment>
   )
 }
