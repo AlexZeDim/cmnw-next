2fd984463790045e1291dd7dfde6773e86e2c894
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2022-10-21 12:14:57 +0300

    refactor(pages): login-area v5 <meta> => Box

--- pages/login-area.tsx
+++ pages/login-area.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Image from 'next/image'
 import MetaHead from '../libs/components/MetaHead';
-import { Container, Grid, makeStyles, Typography } from '@material-ui/core';
+import { Container, Grid, Typography, Box } from '@mui/material';
 
-const useStyles = makeStyles(() => ({
+const styleCss = {
   main: {
     marginTop: '65px',
   },
@@ -20,36 +20,37 @@ const useStyles = makeStyles(() => ({
     left: '50%',
     top: '50%',
   }
-}));
+}
 
 const LoginArea = () => {
-  const classes = useStyles();
   return (
-    <main className={classes.main}>
-      <MetaHead
-        title={'Login Area'}
-        description={'Work in progress'}
-        wowhead={false}
-      />
-      <Container maxWidth={false} className={classes.root}>
-        <Grid container direction="column" justifyContent="center" alignItems="center">
-          <Grid item xs={12} sm={12} md={12}>
-            <Typography variant="h1" component="h5" align="center" gutterBottom>
-              THE NEXT BIG THING
-            </Typography>
+    <main>
+      <Box sx={styleCss.main}>
+        <MetaHead
+          title={'Login Area'}
+          description={'Work in progress'}
+          wowhead={false}
+        />
+        <Container maxWidth={false} sx={styleCss.root}>
+          <Grid container direction="column" justifyContent="center" alignItems="center">
+            <Grid item xs={12} sm={12} md={12}>
+              <Typography variant="h1" component="h5" align="center" gutterBottom>
+                THE NEXT BIG THING
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={12} md={12}>
+              <Image
+                src={'/static/cmnw-1.svg'}
+                alt='CMNW'
+                layout='intrinsic'
+                style={styleCss.image}
+                width={700}
+                height={475}
+              />
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={12} md={12}>
-            <Image
-              src={'/static/cmnw-1.svg'}
-              alt="CMNW"
-              layout="intrinsic"
-              className={classes.image}
-              width={700}
-              height={475}
-            />
-          </Grid>
-        </Grid>
-      </Container>
+        </Container>
+      </Box>
     </main>
   )
 }
